82272f7435ad7522f1f9c6fff34530a1c3339a9f
Roland Kujundzic <roland@inkoeln.com>
2016-01-24 13:52:57 +0100

    test conf2kv()

---
+++ test/lib_conf2kv/conf2kv.fc.php
@@ -0,0 +1,18 @@
+<?php
+
+$func = 'rkphplib\lib\conf2kv';
+
+$e01_conf = trim(file_get_contents(__DIR__.'/example_01.conf'));
+$e01_ok = trim(file_get_contents(__DIR__.'/example_01.ok'));
+
+$test = [
+	[' x y z ', ' x y z '],
+	['" x y z "', ' x y z '],
+	['a|#|', '["a",""]'],
+	['a= 12 |#|', '{"a":"12"}'],
+	['a=A', '{"a":"A"}'],
+	['abc="A""BC"'."\n|#|\n\t b =  B B B", '{"abc":"A\"\"BC","b":"B B B"}'],
+	['a=1|#|b=2|#|c=3|#|', '{"a":"1","b":"2","c":"3"}'],
+	[$e01_conf, $e01_ok]
+];
+
---
+++ test/lib_conf2kv/run.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once(dirname(__DIR__).'/testlib.php');
+require_once(dirname(dirname(__DIR__)).'/src/lib/conf2kv.php');
+
+fc('conf2kv');
