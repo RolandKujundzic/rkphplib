ff1d4d53a872b57812b8fa9fb2563a503760cc77
Roland Kujundzic <roland@inkoeln.com>
2016-01-24 14:02:39 +0100

    initial

---
+++ src/Session.class.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace rkphplib;
+
+require_once(__DIR__.'/Exception.class.php');
+
+use rkphplib\Exception;
+
+
+/**
+ * Session wrapper.
+ *
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ */
+class Session {
+
+}
+
