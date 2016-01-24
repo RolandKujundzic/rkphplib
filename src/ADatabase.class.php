095421d06c3e07e1eb8d9f963f243ef3661f215f
Roland Kujundzic <roland@inkoeln.com>
2016-01-24 20:21:28 +0100

    Database Wrapper

---
+++ src/ADatabase.class.php
@@ -0,0 +1,481 @@
+<?php
+
+namespace rkphplib;
+
+require_once(__DIR__.'/Exception.class.php');
+
+use rkphplib\Exception;
+
+
+
+/**
+ * Abstract database access wrapper class.
+ * 
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ */
+abstract class ADatabase {
+
+/** @var bool $use_prepared */
+public static $use_prepared = true;
+
+protected $_dsn = null;
+
+protected $_query = array();
+
+
+
+/**
+ * Set database connect string. 
+ *
+ * Examples:
+ * mysqli://user:password@tcp+localhost/dbname
+ * sqlite://[password]@path/to/file.sqlite
+ * 
+ * @throws rkphplib\Exception if $dsn is empty or connection is already open and $dsn has changed
+ * @param string $dsn
+ */
+public function setDSN($dsn) {
+
+	if (!$dsn) {
+		throw new Exception('empty database source name');
+	}
+  
+	if (!empty($this->_dsn) && $this->_dsn != $dsn) {
+		throw new Exception('close connection first');
+	}
+
+	$this->_dsn = $dsn;
+}
+
+
+/**
+ * Return database connect string.
+ *
+ * @throws rkphplib\Exception if $dsn was not set 
+ * @return string 
+ */
+public function getDSN() {
+
+	if (empty($this->_dsn)) {
+		throw new Exception('call setDSN() first');
+	}
+
+	return $this->_dsn;
+}
+
+
+/**
+ * Split data source name into hash (type, login, password, protocol, host, port, name, [file]). 
+ *
+ * Example:
+ * type://login:password@protocol+host:port/name 
+ * type://[password]@./file or type://@/path/to/file 
+ *
+ * @throws rkphplib\Exception if split failed
+ * @string $dsn (default = null, use setDSN() value)
+ * @return map
+ */
+public function splitDSN($dsn = null) {
+
+	if (is_null($dsn)) {
+		if (empty($this->_dsn)) {
+			throw new Exception('call setDSN() first');
+		}
+
+		$dsn = $this->_dsn;
+	}
+
+	$file_db = array('sqlite');
+
+	if (preg_match('/^([a-z0-9]+)\:\/\/(.+?)@(.+?)\/(.+)$/i', $this->_dsn, $match)) {
+		$db = array('login' => '', 'password' => '', 'protocol' => '', 'host' => '', 'port' => '', 'name' => '', 'file' => '');
+		$db['type'] = $match[1];
+
+		if (!empty($match[2])) {
+			if (($pos = mb_strpos($match[2], ':')) > 0) {
+				$db['login'] = mb_substr($match[2], 0, $pos);
+				$db['password'] = mb_substr($match[2], $pos + 1);
+			}
+			else {
+				$db['password'] = $match[2];
+			}
+		}
+		
+		if (in_array($db['type'], $file_db)) {
+			$db['file'] = $match[3].'/'.$match[4];
+		}
+		else {
+			if (empty($match[3])) {
+				throw new Exception('empty host, use host|host:port|protocol+host:port');
+			}
+
+			if (($pos = mb_strpos($match[3], '+')) > 0) {
+				$db['protocol'] = mb_substr($match[3], 0, $pos);
+				$match[3] = mb_substr($match[3], $pos + 1);
+			}
+
+			if (($pos = mb_strpos($match[3], ':')) > 0) {
+				$db['host'] = mb_substr($match[3], 0, $pos);
+				$db['port'] = mb_substr($match[3], $pos + 1);
+			}
+			else {
+				$db['host'] = $match[3];
+			}
+
+			$db['name'] = $match[4];
+		}
+	}
+	else {
+		throw new Exception('invalid dsn', $dsn);
+	}
+
+	return $db;
+}
+
+
+/**
+ * Define query with placeholders. 
+ *
+ * Example:
+ * INSERT INTO {:=^table} (a, b, c) VALUES ('{:=a}', '{:=b}', {:=_c})
+ * 
+ * If tag is '{:=x}' use prepared statement value bind (or change into {:=x} if self::$use_prepared = false). 
+ * If tag is {:=x} apply escape($value).
+ * If tag is {:=^x} apply escape_name($value).
+ * If tag is {:=_x} keep $value.
+ *
+ * @throws rkphplib\Exception if error
+ * @param string $qkey
+ * @param string $query
+ */
+public function setQuery($qkey, $query) {
+
+	if (empty($qkey)) {
+		throw new Exception('empty query key');
+	}
+
+	if (empty($query)) {
+		throw new Exception('empty query');
+	}
+
+	if (mb_strpos($query, '{:=') === false) {
+		$this->_query[$qkey] = $query;
+		return;
+	}
+
+	$tok = preg_split("/('?\{:=[a-zA-Z0-9_\^]+\}'?)/s", $query, -1, PREG_SPLIT_DELIM_CAPTURE);
+	// value is: bind, escape, escape2, escape_name, keep
+	$map = array('bind' => array());
+
+	for ($i = 1; $i < count($tok) - 1; $i += 2) {
+		$m = $tok[$i];
+
+		if (mb_substr($m, 0, 1) == "'" && mb_substr($m, -1) == "'" && mb_substr($m, 4, 1) != '^') {
+			if (self::$use_prepared) {
+				array_push($map['bind'], mb_substr($m, 4, -2));
+				$tok[$i] = '?';
+			}
+			else {
+				$key = mb_substr($m, 4, -2);
+				$tok[$i-1] = mb_substr($tok[$i-1], 0, -1);
+				$map[$key] = 'escape';
+				$tok[$i+1] = mb_substr($tok[$i+1], 1);
+			}
+		}
+		else if (mb_substr($m, 3, 1) == '_') {
+			$key = mb_substr($m, 3, -1);
+			$map[$key] = 'keep';
+		}
+		else if (mb_substr($m, 3, 1) == '^') {
+			$key = mb_substr($m, 4, -1);
+			$map[$key] = 'escape_name';
+		}
+		else {
+			$key = mb_substr($m, 3, -1);
+			$map[$key] = 'escape';
+		}
+	}
+
+	if (count($map['bind']) == 0) {
+		unset($map['bind']);
+	}
+
+	$map['@query'] = join('', $tok);
+	$this->_query[$qkey] = $map;
+}
+
+
+/**
+ * Return (prepared) query defined via setQuery($qkey, '...').
+ *
+ * If query is prepared return parameter array (last two entries are query and replace hash). 
+ *
+ * @throws rkphlib\Exception if error
+ * @param string $qkey
+ * @param hash $replace
+ * @return string|vector
+ */
+public function getQuery($qkey, $replace = null) {
+
+	if (!isset($this->_query[$qkey])) {
+		throw new Exception('call setQuery() first', "qkey=[$qkey]");
+	}
+
+	$q = $this->_query[$qkey];
+
+	if (!is_array($q)) {
+		if (is_array($replace) && count($replace) > 0) {
+			throw new Exception('query has no tags', print_r($q, true));
+		}
+
+		// return text query ...
+		return $q;
+	}
+
+	$query = $q['@query'];
+	unset($q['@query']);
+
+	$bind = null;
+	if (isset($q['bind']) && count($q['bind']) > 0) { 
+		$bind = $q['bind'];
+		unset($q['bind']);
+	}
+
+	foreach ($q as $key => $do) {
+		if (!isset($replace[$key])) {
+			throw new Exception("query replace key $key missing", "($qkey) $query: ".print_r($replace, true));
+		}
+
+		if ($do == 'escape') {
+			$value = $replace[$key];
+
+			if (is_null($value) || $value == 'NULL' || $value == 'null') {
+				$value = 'NULL';
+			}
+			else {
+				$value = "'".$this->esc($value)."'";
+			}
+
+			$query = str_replace("{:=$key}", $value, $query);
+		}
+		else if ($do == 'escape_name') {
+			$query = str_replace('{:=^'.$key.'}', self::escape_name($replace[$key]), $query);
+		}
+		else if ($do == 'keep') {
+			$query = str_replace('{:='.$key.'}', $replace[$key], $query);
+		}
+		else {
+			throw new Exception("Unknown replace action", "do=$do query=$query");
+		}
+
+		unset($replace[$key]);
+	}
+
+	if (!$bind) {
+		if (count($replace) > 0) {
+			throw new Exception("Too many replace parameter", print_r($replace, true));
+		}
+
+		return $query;
+	}
+
+	array_push($bind, $query);
+	array_push($bind, $replace);
+
+	return $bind;
+}
+
+
+/**
+ * True if query key exists.
+ * 
+ * @param string $qkey
+ * @return boolean
+ */
+public function hasQuery($qkey) {
+	return isset($this->_query[$qkey]) ? false : true;
+}
+
+
+/**
+ * Apply setQuery() for all hash values where key has [query.] prefix and value is not empty (qkey = key without prefix).
+ *
+ * @see ADatabse::setQuery()
+ * @throws rkphplib\Exception if error
+ * @param map $conf_hash
+ * @param empty|array $require_keys
+ */
+public function setQueryHash($conf_hash, $require_keys = '') {
+
+	if (is_array($require_keys)) {
+		foreach ($require_keys as $qkey) {
+			if (empty($conf_hash['query.'.$qkey]) && empty($this->_query[$qkey])) {
+				throw new Exception("no such key [query.$qkey]");
+			}
+		}
+	}
+
+	foreach ($conf_hash as $key => $value) {
+		if (mb_substr($key, 0, 6) == 'query.' && !empty(trim($value))) {
+			$this->setQuery(mb_substr($key, 6), $value);
+		}
+	}
+}
+
+
+/**
+ * Escape table or column name with `col name`.
+ *
+ * If abort is true abort if name doesn't match [a-zA-Z0-9_\.]+.
+ *
+ * @throws rkphplib\Exception if error
+ * @param string
+ * @return string
+ */
+public static function escape_name($name, $abort = false) {
+	$res = $name;
+
+	if (!preg_match("/^[a-zA-Z0-9_\.]+$/", $name)) {
+		if ($abort) {
+			throw new Exception('invalid sql name', $name);
+		}
+
+		if (mb_strpos($name, '`') !== false) {
+			throw new Exception('invalid sql name', $name);
+		}
+
+		$res = '`'.$name.'`';
+	}
+
+	return $res;
+}
+
+
+/**
+ * Apply database specific escape function (fallback is self::escape).
+ *
+ * @param string $txt
+ * @return string
+ */
+abstract public function esc($txt);
+
+
+/**
+ * Execute query (string or prepared statement).
+ *
+ * @param string|object $query
+ */
+abstract public function execute($query);
+
+
+/**
+ * Return column values.
+ *
+ * @param string $query
+ * @param string $colname (default = col)
+ * @return array
+ */
+abstract public function selectColumn($query, $colname = 'col');
+
+
+/**
+ * Return table description. 
+ *
+ * Result hash keys are column names, values are arrays with column info
+ * (e.g. mysql: { Type: 'double', Null: 'YES', Key: '', Default: '', Extra: '' }).
+ *
+ * @param string $table
+ * @return map
+ */
+abstract public function getTableDesc($table);
+
+
+/**
+ * Return hash values (key, value columns).
+ *
+ * @param string $query
+ * @param string $key_col (default = name)
+ * @param string $value_col (defualt = value)
+ * @param bool $ignore_double (default = false)
+ * @return hash
+ */
+abstract public function selectHash($query, $key_col = 'name', $value_col = 'value', $ignore_double = false);
+
+
+/**
+ * Return query result row $rnum.
+ *
+ * @param string $query
+ * @param int $rnum (default = 0)
+ * @return hash
+ */
+abstract public function selectRow($query, $rnum = 0);
+
+
+/**
+ * Return query result table. 
+ *
+ * If $res_count > 0 throw error if column count doesn't match.
+ *
+ * @param string $query
+ * @param int $res_count (default = 0)
+ * @return table
+ */
+abstract public function select($query, $res_count = 0);
+
+
+/**
+ * Set offset for following select* function.
+ *
+ * @param int $offset
+ */
+public function seek($offset) {
+	$this->_seek = $offset;
+}
+
+
+/**
+ * Return query hash (single row).
+ * 
+ * @throw rkphplib\Exception if rownum != 1 
+ * @param string $query
+ */
+public function selectOne($query) {
+	$dbres = $this->select($query, 1);
+	return $dbres[0];
+}
+
+
+/**
+ * Escape ' with ''. 
+ * 
+ * Append \ to trailing uneven number of \. 
+ * 
+ * @param string $txt
+ * @return string
+ */
+public static function escape($txt) {
+
+	if (mb_substr($txt, -1) == '\\') {
+		// trailing [\'] is a problem because \ is mysql escape char
+		$l = mb_strlen($txt) * -1;
+		$n = -1;
+
+		while (mb_substr($txt, $n, 1) == '\\' && $n > $l) {
+			$n--;
+		}
+
+		if ($n % 2 == 0) {
+			$txt .= '\\';
+		}
+	}
+
+	$res = str_replace('´', "'", $txt);
+	$res = str_replace("'", "''", $res);
+
+	return $res;
+}
+
+
+}
+
---
+++ src/MysqlDatabase.class.php
@@ -0,0 +1,479 @@
+<?php
+
+namespace rkphplib;
+
+require_once(__DIR__.'/ADatabase.class.php');
+
+use rkphplib\lib\Exception;
+
+
+
+/**
+ * MysqlDatabase implementation of ADatabase.
+ *
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ */
+class MysqlDatabase extends ADatabase {
+
+private $_db = null;
+private $_conn_ttl = 0;
+private $_seek = -1;
+private $_cache = array();
+
+
+
+/**
+ * Connect to mysql database.
+ */
+private function _connect() {
+
+  if (is_object($this->_db)) {
+		if ($this->_conn_ttl < time() && !$this->_db->ping()) {
+			$this->close();
+		}
+		else {
+	    return;
+		}
+  }
+
+  if (empty($this->_dsn)) {
+		throw new Exception('call setDSN first');
+  }
+
+	$dsn = $this->splitDSN();
+
+	if ($dsn['type'] != 'mysqli') {
+		throw new Exception('invalid dsn type: '.$dsn['type']);
+	}
+
+	if (!empty($dsn['port'])) {
+		$this->_db = mysqli_init();
+		if (!$this->_db->real_connect($dsn['host'], $dsn['login'], $dsn['password'], $dsn['name'], $dsn['port'])) {
+			throw new Exception('Failed to connect to MySQL ('.$this->_db->connect_errno.')', $this->_db->connect_error);
+		}
+	}
+	else {
+		$this->_db = new \mysqli($dsn['host'], $dsn['login'], $dsn['password'], $dsn['name']);
+		if ($this->_db->connect_errno) {
+			throw new Exception('Failed to connect to MySQL ('.$this->_db->connect_errno.')', $this->_db->connect_error);
+		}
+	}
+
+	if (!$this->_db->set_charset('utf8mb4')) {
+		throw new Exception('set charset utf8mb4 failed');
+	}
+
+	$this->_conn_ttl = time() + 5 * 60; // re-check connection in 5 minutes ...
+}
+
+
+/**
+ * Close database connection.
+ */
+public function close() {
+
+	if (!$this->_db) {
+		throw new Exception('no open database connection');
+	}
+
+	if (!$this->_db->close()) {
+		throw new Exception('failed to close database connection');
+	}
+
+	foreach ($this->_query as $key) {
+		if (is_object($this->_query[$key])) {
+			if (!$this->_query[$key]->close()) {
+				throw new Exception('failed to close prepared statement', $key);
+			}
+		}
+	}
+
+	$this->_db = null;
+	$this->_conn_ttl = 0;
+}
+
+
+/**
+ * Execute database query.
+ *
+ * @param string $query
+ */
+public function execute($query) {
+
+	if (is_array($query)) {
+		$stmt = $this->_exec_stmt($query);
+		$stmt->close();
+	}
+	else {
+		$this->_connect();
+
+		if ($this->_db->real_query($query) === false) {
+			throw new Exception('failed to execute sql query', $query."\n(".$this->_db->errno.') '.$this->_db->error);
+		}
+	}
+}
+
+
+/**
+ * Execute database query $query and return result column $col.
+ *
+ * @param string $query
+ * @param string $col
+ * @return vector
+ */
+public function selectColumn($query, $colname = 'col') {
+	
+	if (is_array($query)) {
+		$res = $this->_fetch_stmt($this->_exec_stmt($query), array($colname)); 
+	}
+	else {
+		$res = $this->_fetch($query, array($colname));
+	}
+
+	return $res;
+}
+
+
+/**
+ * Execute database query and return map.
+ * 
+ * @param string $query
+ * @param string $key_col
+ * @param string $value_col
+ * @param bool $ignore_double
+ * @return map
+ */
+public function selectHash($query, $key_col = 'name', $value_col = 'value', $ignore_double = false) {
+
+	if (is_array($query)) {
+		$res = $this->_fetch_stmt($this->_exec_stmt($query), array($key_col, $value_col)); 
+	}
+	else {
+		$res = $this->_fetch($query, array($key_col, $value_col));
+	}
+
+	if (!$ignore_double && isset($this->_cache['FETCH:DOUBLE'])) {
+		throw new Exception('Hashkeys are not unique', print_r($this->_cache['FETCH:DOUBLE'], true));
+	}
+
+	return $res;
+}
+
+
+/**
+ * Execute select query.
+ *
+ * @param string $query 
+ * @param int $res_count
+ * @return table
+ */
+public function select($query, $res_count = 0) {
+
+	if (is_array($query)) {
+		$res = $this->_fetch_stmt($this->_exec_stmt($query), null, $res_count); 
+	}
+	else {
+		$res = $this->_fetch($query, null, $res_count);
+	}
+
+	return $res;
+}
+
+
+/**
+ * Execute select query and fetch result. 
+ * 
+ * Return result table if $rbind = null.
+ * Return hash if count($rbind) = 2. Return array if count($rbind) = 1.
+ * Return hash if $rcount < 0 (result[-1 * $rcount + 1]).
+ *
+ * @param string $query
+ * @param array $rbind (default = null)
+ * @param int $rcount (default = 0)
+ * @return table|map|vector
+ */
+private function _fetch($query, $rbind = null, $rcount = 0) {
+
+	$this->_connect();
+
+	if (($dbres = $this->_db->query($query)) === false) {
+		throw new Exception('failed to execute sql query', $query."\n(".$this->_db->errno.') '.$this->_db->error);
+	}
+
+	$rnum = $dbres->num_rows;
+	$res = array();
+
+	if ($rcount > 0 && $rnum != $rcount) {
+		throw new Exception('unexpected number of rows', $rnum.' != '.$rcount);
+	}
+
+	if ($rcount < 0 && -1 * $rcount > $rnum) {
+		throw new Exception('number of rows too low', $rnum.' < '.(-1 * $rcount));
+	}
+
+	if ($rnum > 50000) {
+		throw new Exception('number of rows too high', $rnum);
+	}
+
+	if ($rnum == 0) {
+		return $res;
+	}
+
+	$end = $rnum;
+
+  if ($rcount < 0) {
+    $dbres->data_seek(-1 * $rcount + 1);
+    $end = 1;
+  }
+  else if ($this->_seek > -1) {
+    $dbres->data_seek($this->_seek);
+    $end -= $this->_seek + 1;
+    $this->_seek = -1;
+  }
+
+	$bl = is_null($rbind) ? 0 : count($rbind);
+
+	for ($i = 0; $i < $end; $i++) {
+		$dbrow = $dbres->fetch_assoc();
+
+		if ($bl == 1) {
+			array_push($res, $dbrow[$rbind[0]]);
+		}
+		else if ($bl == 2) {
+			$key = $dbrow[$rbind[0]];
+
+			if (isset($res[$key])) {
+				if (!isset($this->_cache['FETCH:DOUBLES'])) {
+					$this->_cache['FETCH:DOUBLE'] = array();
+				}
+
+				$this->_cache['FETCH:DOUBLE'][$key]++;
+			}
+
+			$res[$key] = $dbrow[$rbind[1]];
+		}
+		else {
+			array_push($res, $dbrow);
+		}
+	}
+
+	$dbres->close();
+
+	return $res;
+}
+
+
+/**
+ * Execute query and return result row $rnum.
+ * 
+ * @param string $query
+ * @param int $rnum
+ * @return map
+ */
+public function selectRow($query, $rnum = 0) {
+
+	$rnum = -1 * $rnum - 1;
+
+	if (is_array($query)) {
+		$res = $this->_fetch_stmt($this->_exec_stmt($query), null, $rnum); 
+	}
+	else {
+		$res = $this->_fetch($query, null, $rnum);
+	}
+
+	return $res;
+}
+
+
+/**
+ * Execute prepared statement. 
+ * 
+ * Return statement.
+ *
+ * @param array $q
+ * @return object 
+ */
+private function _exec_stmt($q) {
+
+	$this->_connect();
+
+	$ql = count($q);
+	$replace = $q[$ql - 1];
+	$query = $q[$ql - 2];
+
+	if (!($stmt = $this->_db->prepare($query))) {
+		throw new Exception("Prepare query failed", $query."\n(".$this->_db->errno.') '.$this->_db->error."\n$query");
+	}
+
+	$bind_arr = array('');
+
+	for ($i = 0; $i < $ql - 2; $i++) {
+		$key = $q[$i];
+
+		if (!isset($replace[$key])) {
+			throw new Exception("query replace key $key missing", "$query: ".print_r($replace, true));
+		}
+
+		$bind_arr[0] .= 's';
+		$bind_arr[$i + 1] =& $replace[$key]; 
+		unset($replace[$key]);
+	}
+
+	$ref = new \ReflectionClass('mysqli_stmt');
+	$method = $ref->getMethod('bind_param');
+
+	if ($method->invokeArgs($stmt, $bind_arr) === false) {
+		throw new Exception('query bind parameter failed', "$query\n(".$stmt->errno.') '.$stmt->error."\n".print_r($bind_arr, true));
+	}
+
+	if (count($replace) > 0) {
+		throw new Exception('Too many replace parameter', print_r($replace, true)); 
+	}
+
+	// prepared statement ...
+	if (!$stmt->execute()) {
+		throw new Exception('failed to execute sql statement');
+	}
+
+	return $stmt;
+}
+
+
+/**
+ * Fetch prepared statement result. 
+ *
+ * Return result table if $rbind = null.
+ * Return hash if count($rbind) = 2. Return array if count($rbind) = 1.
+ * Return hash if $rcount < 0 (result[-1 * $rcount + 1]).
+ *
+ * @param object $stmt
+ * @param array $rbind (default = null)
+ * @param int $rcount (default = 0)
+ * @return table|map|vector
+ */
+private function _fetch_stmt($stmt, $rbind = null, $rcount = 0) {
+
+	if (!$stmt->store_result()) {
+		throw new Exception('failed to store result');
+	}
+
+	$rnum = $stmt->num_rows;
+	$res = array();
+
+	if ($rcount > 0 && $rnum != $rcount) {
+		throw new Exception('unexpected number of rows', $rnum.' != '.$rcount);
+	}
+
+	if ($rcount < 0 && -1 * $rcount > $rnum) {
+		throw new Exception('number of rows too low', $rnum.' < '.(-1 * $rcount));
+	}
+
+	if ($rnum > 50000) {
+		throw new Exception('number of rows too high', $rnum);
+	}
+
+	if ($rnum == 0) {
+		return $res;
+	}
+
+	if (is_null($rbind)) {
+		$md = $stmt->result_metadata();
+		$rbind = array();
+		$db_data = array();
+		$db_refs = array(); // db_refs is necessary because call_use_func_array needs array with references
+
+		while (($field = $md->fetch_field())) {
+			$db_refs[] =& $db_data[$field->name]; // bind db_refs[n] to db_data[key]
+			array_push($rbind, $field->name);
+		}
+	}
+
+	$end = $rnum;
+
+	if ($rcount < 0) {
+		$stmt->data_seek(-1 * $rcount + 1);
+		$end = 1;
+	}
+	else if ($this->_seek > -1) {
+		$stmt->data_seek($this->_seek);
+		$end -= $this->_seek + 1;
+		$this->_seek = -1;
+	}
+
+	if (call_user_func_array(array($stmt, 'bind_result'), $db_refs) === false) {
+		throw new Exception('failed to bind result');
+	}
+
+	$bl = count($rbind);
+
+	for ($i = 0; $i < $end; $i++) {
+		$stmt->fetch();
+
+    if ($bl == 1) {
+      array_push($res, $db_data[$rbind[0]]);
+    }
+    else if ($bl == 2) {
+      $res[$db_data[$rbind[0]]] = $db_data[$rbind[1]];
+    }
+    else if ($bl > 2) {
+			$row = array();
+
+			foreach ($rbind as $key) {
+				$row[$key] = $db_data[$key];
+			}
+
+			array_push($res, $row);
+		}
+  }
+
+	$stmt->close();
+
+	return $res;
+}
+
+
+/**
+ * Return escaped value.
+ * 
+ * @param string $txt 
+ * @return string
+ */
+public function esc($txt) {
+
+	if (!$this->_db) {
+		return self::escape($txt);
+	}
+
+	return $this->_db->real_escape_string($txt);	
+}
+
+
+/**
+ * Return table description.
+ * 
+ * @param string $table
+ * @return map
+ */
+public function getTableDesc($table) {
+
+	if (isset($this->_cache['DESC:'.$table])) {
+    return $this->_cache['DESC:'.$table];
+  }
+
+  $db_res = $this->select('DESC '.self::escape_name($table));
+  $res = array();
+
+  foreach ($db_res as $info) {
+    $colname = $info['Field'];
+    unset($info['Field']);
+    $res[$colname] = $info;
+  }
+
+  $this->_cache['DESC:'.$table] = $res;
+
+  return $res;
+}
+
+
+}
+
