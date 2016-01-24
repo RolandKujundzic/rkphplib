edcb84c7bfabaacb1135a35f9e26994a05c23b0d
Roland Kujundzic <roland@inkoeln.com>
2016-01-24 14:02:33 +0100

    initial

---
+++ src/TLogin.class.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace rkphplib;
+
+require_once(__DIR__.'/Session.class.php');
+
+use rkphplib\Exception;
+
+
+/**
+ * Tokenizer Login/Session plugin.
+ *
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ *
+ */
+class TLogin {
+
+
+/**
+ * Initialize/Check login session.
+ *
+ * @param map $p
+ */
+public function tok_login_init($p) {
+}
+
+
+/**
+ * Return login key value.
+ *
+ * @param string $key
+ * @return string
+ */
+public function tok_login($key) {
+}
+
+}
