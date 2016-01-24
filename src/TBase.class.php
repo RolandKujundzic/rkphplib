e9c491561f42c9c1e470b05b1fd5360c853dc020
Roland Kujundzic <roland@inkoeln.com>
2016-01-24 14:02:22 +0100

    initial

---
+++ src/TBase.class.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace rkphplib;
+
+require_once(__DIR__.'/Exception.class.php');
+require_once(__DIR__.'/File.class.php');
+
+use rkphplib\Exception;
+
+
+/**
+ * Basic Tokenizer plugins.
+ *
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ */
+class TBase {
+
+}
