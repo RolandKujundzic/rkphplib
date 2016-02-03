17aaae6f6c150d83aa7a54399ce5de42f3549f76
Roland Kujundzic <roland@inkoeln.com>
2016-02-04 00:37:21 +0100

    initial

---
+++ src/lib/split_str.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace rkphplib\lib;
+
+
+/**
+ * Split string at delimiter.
+ *
+ * Remove quotes from parts, allow backslash escaped delimiter char.
+ *
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ * @param string $delim
+ * @param string $txt
+ * @param boolean $ignore_empty (default = false)
+ * @param int $limit (default = -1)
+ * @return array
+ */
+function split_str($delim, $txt, $ignore_empty = false, $limit = -1) {
+
+	$esc = (strlen($delim) == 1) ? '\\' : '';
+	$len = strlen($txt);
+	$is_esc = false;
+	$parts = array();
+	$lpos = 0;
+	$pos = 0;
+
+	while ($pos < $len && ($pos = strpos($txt, $delim, $pos)) !== false) {
+
+		if ($esc && substr($txt, $pos - 1, 1) == $esc) {
+			$is_esc = true;
+			$pos++;
+    }
+		else {
+			$value = trim(substr($txt, $lpos, $pos - $lpos));
+
+			if ($is_esc) {
+				$value = str_replace($esc.$delim, $delim, $value);
+			}
+
+			if (substr($value, 0, 1) == '"' && substr($value, -1) == '"' && strlen($value) > 1) {
+				$value = substr($value, 1, -1);
+			}
+
+			if (!$ignore_empty || strlen($value) > 0) {
+				array_push($parts, $value);
+			}
+
+			$pos++;
+			$lpos = $pos;
+			$is_esc = false;
+		}
+	}
+
+	if ($lpos < $len) {
+		$value = trim(substr($txt, $lpos));
+
+		if ($is_esc) {
+			$value = str_replace($esc.$delim, $delim, $value);
+		}
+
+		if (substr($value, 0, 1) == '"' && substr($value, -1) == '"' && strlen($value) > 1) {
+			$value = substr($value, 1, -1);
+		}
+
+		if (!$ignore_empty || strlen($value) > 0) {
+			array_push($parts, $value);
+		}
+	}
+	else if ($lpos == $len && !$ignore_empty && substr($txt, -1) === $delim) {
+		// a,b, = [a][b][]
+		array_push($parts, '');
+	}
+
+	if ($limit > 0 && count($parts) > $limit) {
+		$tmp = array();
+
+		while ($limit > 1) {
+			array_push($tmp, array_shift($parts));
+			$limit--;
+		}
+
+		array_push($tmp, join($delim, $parts));
+		return $tmp;
+	}
+
+	return $parts;
+}
+
