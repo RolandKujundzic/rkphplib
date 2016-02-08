101baaae128f299eb4b7892bf7464f4116e60111
Roland Kujundzic <roland@inkoeln.com>
2016-02-08 02:17:46 +0100

    initial

---
+++ src/lib/log_debug.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace rkphplib\lib;
+
+require_once(__DIR__.'/config.php');
+
+
+/**
+ * Log debug message.
+ *
+ * Disable debug log with $settings_LOG_DEBUG = 0,
+ * Enable logging to default with $settings_LOG_DEBUG = 1 (default).
+ * Enable logging to file with $settings_LOG_DEBUG = 'path/debug.log'.
+ *
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ * @param string $msg
+ * @param bool $prepend_info (default = false, prepend timestamp and trace information)
+ */
+function log_debug($msg, $prepend_info = false) {
+	global $settings_LOG_DEBUG;
+
+	if (!$settings_LOG_DEBUG) {
+		return;
+	}
+
+	if ($prepend_info) {
+		list($msec, $ts) = explode(" ", microtime());
+		$log = '['.date('YdmHis', $ts).'.'.(1000 * round((float)$msec, 3));
+
+		if (!empty($_SERVER['REMOTE_ADDR'])) { 
+			$log .= $_SERVER['REMOTE_ADDR'];
+		}
+
+		if (isset($_SERVER['SCRIPT_FILENAME']) && isset($_SERVER['QUERY_STRING'])) {
+		  $log .= '] '.$_SERVER['SCRIPT_FILENAME'].$_SERVER['QUERY_STRING']."\n$msg";
+		}
+		else {
+			$log .= "] $msg";
+		}
+	}
+	else {
+		$log = $msg;
+	}
+
+	if (mb_strlen($settings_LOG_DEBUG) > 1) {
+		error_log($log."\n", 3, $settings_LOG_DEBUG);
+	}
+	else {
+		error_log($log);
+	}
+}
+
---
+++ test/lib_log_debug/todo.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(dirname(dirname(__DIR__)).'/src/lib/log_debug.php');
+
+function a() { rkphplib\lib\log_debug('debug 1'); }
+
+class B {
+	public function __construct() { $this->_x(); }
+	private function _x() { rkphplib\lib\log_debug('debug 2'); }
+}
+
+a();
+$b = new B();
+
