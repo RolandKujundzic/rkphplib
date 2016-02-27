87a08ef0e7035eefc7c6da2c6ed2405828f99a70
Roland Kujundzic <roland@inkoeln.com>
2016-02-27 12:08:18 +0100

    Adjust to refactoring

--- test/CryptoHelper/todo.php
+++ test/CryptoHelper/todo.php
@@ -3,27 +3,52 @@
 require_once(dirname(dirname(__DIR__)).'/src/CryptoHelper.class.php');
 
 
-// $method = [ 'xor', 'ord', 'aes256cbc', 'mcrypt_r256' ];
-$method = [ 'xor', 'ord', 'mcrypt_r256' ];
-$wrapper = [ 'no', 'urlenc_base64', 'safe64' ]; 
+/**
+ *
+ */
+function _enc_dec($opt) {
+	$ox = empty($opt['xor_secret']) ? '' : ' obfuscated';
 
-$txt = 'This is a secret.';
-$secret = 'no one must know';
+	foreach ($opt['method'] as $m) {
+		foreach ($opt['wrapper'] as $w) {
+			$opt['wrap'] = $w;
+			$opt['enc'] = $m;
 
-print "\n\nEn/Decode [$txt] with secret [$secret]:\n\n";
+			$ch = new rkphplib\CryptoHelper($opt);
+			$enc = $ch->encode($opt['txt']);
+			print "encode$ox: $w($m(...)) = [$enc]\n";
+			$dec = $ch->decode($enc);
+			print "decode$ox: $m($w(...)) = [$dec]\n\n";
 
-foreach ($method as $m) {
-	foreach ($wrapper as $w) {
-		$ch = new rkphplib\CryptoHelper(array('secret' => $secret, 'enc' => $m, 'wrap' => $w));
-		$enc = $ch->encode($txt);
-		print "encode: $w($m(...)) = [$enc]\n";
-		$dec = $ch->decode($enc);
-		print "decode: $m($w(...)) = [$dec]\n\n";
-
-		if ($dec !== $txt) {
-			print "ERROR: en/decode failed!\n\n";
-			exit(1);
+			if ($dec !== $opt['txt']) {
+				print "ERROR: en/decode failed!\n\n";
+				exit(1);
+			}
 		}
 	}
 }
 
+
+$opt = array();
+$opt['method'] = [ 'xor', 'ord' ]; 
+$opt['wrapper'] = [ 'no', 'urlenc_base64', 'safe64' ]; 
+$opt['txt'] = 'This is a secret.';
+$opt['secret'] = 'no one must know';
+_enc_dec($opt);
+
+$opt['xor_secret'] = 'aJE13Lm';
+_enc_dec($opt);
+
+$opt['method'] = [ 'aes256cbc' ];
+$opt['xor_secret'] = '';
+_enc_dec($opt);
+
+$opt['xor_secret'] = 'aJE13Lm';
+_enc_dec($opt);
+
+$opt['method'] = [ 'mcrypt_r256' ];
+$opt['xor_secret'] = '';
+_enc_dec($opt);
+
+$opt['xor_secret'] = 'aJE13Lm';
+_enc_dec($opt);
