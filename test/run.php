4b98cd4c0dc73a3fb35c61044a2115dd9591966f
Roland Kujundzic <roland@inkoeln.com>
2016-03-07 22:26:35 +0100

    more tests added

--- test/run.php
+++ test/run.php
@@ -2,9 +2,12 @@
 
 require_once(__DIR__.'/testlib.php');
 
-run_test('lib_csv_explode/run.php');
 run_test('lib_conf2kv/run.php');
+run_test('lib_split_str/run.php');
+run_test('lib_csv_explode/run.php');
+run_test('lib_replace_tags/run.php');
 run_test('DateTime/run.php');
+// run_test('MysqlDatabase/run.php');
 
 $overall = "Overall result of ".count($test_count['overview'])." Class/Function Tests:";
 printf("%s\n%'=".mb_strlen($overall)."s\n", $overall, '');
