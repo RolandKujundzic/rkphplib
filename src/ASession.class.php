26f43bddbae012db6b2902870aa6996e961d9c3e
Roland Kujundzic <roland@inkoeln.com>
2016-09-12 11:41:01 +0200

    refactoring

--- src/ASession.class.php
+++ src/ASession.class.php
@@ -15,12 +15,12 @@ use rkphplib\Exception;
 abstract class ASession {
 
 /** @var map $conf */
-protected $conf = [ 'name' => '',
+protected $conf = [ 'name' => 'ASession',
+	'type' => 'ASession',
 	'host' => '',
 	'script' => '',
 	'docroot' => '',
-	'scope' => '',
-	'type' => '',
+	'scope' => 'docroot',
 	'ttl' => 7200,
 	'expire' => 3600,
 	'max_duration' => 172000,
@@ -30,31 +30,59 @@ protected $conf = [ 'name' => '',
 
 
 /**
- * Set session configuration. Parameter:
+ * Set session configuration. Use empty map as $conf to initialize. Parameter:
  *
- *  name: Session Name (default = empty)
- *  script: $_SERVER[SCRIPT_FILENAME] (set when first called)
- *  host: $_SERVER[HTTP_HOST] (set when first called)
- *  docroot: $_SERVER[DOCUMENT_ROOT] (set when first called)
- *  scope: file|dir|subdir|host|docroot (default = empty)
- *	type: login type or group (default = empty)
+ *  name: Session Name (default = ASession) - required
+ *	type: login type/group (default = ASession) - required
+ *  scope: file|dir|subdir|host|docroot (default = docroot)
  *  ttl: expiration date increase [1-14400] after activity (default = 7200 sec = 2 h)
  *  expire: expiration after inactivity [1-14400] (default = 3600 = 1 h)
  *	max_duration: maximum session duration [1, 345600] (default = 172000 = 4 h)
  * 
+ *  Internal Parameter (set when first called):
+ *
+ *  script: $_SERVER[SCRIPT_FILENAME]
+ *  host: $_SERVER[HTTP_HOST]
+ *  docroot: $_SERVER[DOCUMENT_ROOT]
+ *  start: now()
+ *  lchange: now()
+ *  reload: now()
+ *
  * @throws rkphplib\Exception if check fails
  * @param map $conf
  */
 public function setConf($conf) {
 
-	$allow_scope = array('file', 'dir', 'subdir', 'host', 'docroot');
-	if (isset($conf['scope']) && !in_array($conf['scope'], $allow_scope)) {
-		throw new Exception('no such scope', $conf['scope']);
+	$default = [ 'name' => 'ASession', 'type' => 'ASession', 'scope' => 'docroot', 'ttl' => 7200, 'expire' => 3600, 'max_duration' => 172000 ];
+	$default['script'] = $_SERVER['SCRIPT_FILENAME'];
+	$default['docroot'] = $_SERVER['DOCUMENT_ROOT'];
+	$default['host'] = $_SERVER['HTTP_HOST'];
+	$default['start'] = time();
+	$default['reload'] = time();
+	$default['lchange'] = time();
+
+	foreach ($default as $key => $value) {
+		if (empty($this->conf[$key])) {
+			$this->conf[$key] = $value;
+		}
 	}
 
-	$key_list = [ 'name', 'type', 'scope' ];
+	if (isset($conf['scope'])) {
+		$allow_scope = array('file', 'dir', 'subdir', 'host', 'docroot');
+		if (!in_array($conf['scope'], $allow_scope)) {
+			throw new Exception('no such scope', $conf['scope']);
+		}
+
+		$this->conf['scope'] = $conf['scope'];
+	}
+
+	$key_list = [ 'name', 'type' ];
 	foreach ($key_list as $key) {
 		if (isset($conf[$key])) {
+			if (empty($conf[$key])) {
+				throw new Exception($key.' is empty');
+			}
+
 			$this->conf[$key] = $conf[$key];
 		}
 	}
@@ -81,22 +109,9 @@ public function setConf($conf) {
  * @return string
  */
 public function getSessionKey() {
-	$name = empty($this->conf['name']) ? 'ASession' : $this->conf['name'];
-	$scope = 'any';
+	$scope = '';
 
-	if (empty($this->conf['script'])) {
-		$this->conf['script'] = $_SERVER['SCRIPT_FILENAME'];
-	}
-
-	if (empty($this->conf['host'])) {
-		$this->conf['host'] = $_SERVER['HTTP_HOST'];
-	}
-
-	if (empty($this->conf['docroot'])) {
-		$this->conf['docroot'] = $_SERVER['DOCUMENT_ROOT'];
-	}
-
-  switch ($conf['scope']) {
+  switch ($this->conf['scope']) {
 		case 'file':
       if ($_SERVER['SCRIPT_FILENAME'] == $this->conf['script']) {
 				$scope = md5($_SERVER['SCRIPT_FILENAME']);	
@@ -119,23 +134,23 @@ public function getSessionKey() {
 			break;
 		case 'docroot':
 			if ($this->conf['docroot'] == $_SERVER['DOCUMENT_ROOT']) {
-				$scope = md5($this->conf['docroot']);
+				$scope = md5($this->conf['docroot'].'/*');
 			}
 			break;
 	}
 
-	if (!empty($conf['scope']) && $scope == 'any') {
+	if (empty($scope)) {
 		throw new Exception('invalid scope', print_r($this->conf, true));
 	}
 
-	return md5($name.':'.$scope);
+	return md5($this->conf['name'].':'.$this->conf['type'].':'.$scope);
 }
 
 
 /**
  * Initialize session.
  *
- * Call setConf() or use $conf to initialize session.
+ * Call setConf() or use $conf for custom session.
  *
  * @param map $conf (default = array())
  * @throws rkphplib\Exception if error
