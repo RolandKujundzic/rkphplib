e13d82917e9d38dbc5818231f8682aeca39d5b31
Roland Kujundzic <roland@inkoeln.com>
2016-10-04 10:58:13 +0200

    wrap session tests with ob_*

--- test/run.php
+++ test/run.php
@@ -3,6 +3,14 @@
 require_once(dirname(__DIR__).'/src/TestHelper.class.php');
 
 $th = new rkphplib\TestHelper();
+
+// ob_* is necessary because of Session/run.php
+ob_start();
+$th->runTest('Session/run.php');
+$res = ob_get_contents();
+ob_end_clean();
+print $res;
+
 $th->runTest('lib_conf2kv/run.php');
 $th->runTest('lib_kv2conf/run.php');
 $th->runTest('lib_split_str/run.php');
@@ -11,8 +19,7 @@ $th->runTest('lib_replace_tags/run.php');
 $th->runTest('DateTime/run.php');
 $th->runTest('Tokenizer/run.php');
 $th->runTest('Dir/run.php');
-$th->runTest('TBase/run.php'); // ToDo: track exception as ok
-$th->runTest('Session/run.php');
+$th->runTest('TBase/run.php');
 $th->result();
 
 // ToDo ...
