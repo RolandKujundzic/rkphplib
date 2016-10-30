e21e2e74209dd88f33d87f77d75e58ff9dd5761c
Roland Kujundzic <roland@inkoeln.com>
2016-10-30 16:55:04 +0100

    un|escape test added

--- test/Tokenizer/run.php
+++ test/Tokenizer/run.php
@@ -7,4 +7,16 @@ if (!isset($th)) {
 	$th = new rkphplib\TestHelper();
 }
 
+require_once(dirname(dirname(__DIR__)).'/src/Tokenizer.class.php');
+
+$tx = new rkphplib\Tokenizer();
+
+
+$th->compare('escape', [ $tx->escape('abc efg'), $tx->escape('a{b}c'), $tx->escape('{:=c} {x:} {aa:bb} {:aa}') ], 
+	[ 'abc efg', 'a{b}c', '&#123;&#58;=c&#125; &#123;x&#58;&#125; &#123;aa&#58;bb&#125; &#123;&#58;aa&#125;' ]);
+
+$th->compare('unescape', [ $tx->unescape('abc efg'), $tx->unescape('a{b}c'), 
+	$tx->unescape('&#123;&#58;=c&#125; &#123;x&#58;&#125; &#123;aa&#58;bb&#125; &#123;&#58;aa&#125;') ],
+	[ 'abc efg', 'a{b}c', '{:=c} {x:} {aa:bb} {:aa}' ]);
+
 $th->runTokenizer(1, array());
