0e45cce17d6450aa7c6527f5d3ce863ed0ed1663
Roland Kujundzic <roland@inkoeln.com>
2016-11-26 16:18:16 +0100

    setQueryHash() within getInstance()

--- src/Database.class.php
+++ src/Database.class.php
@@ -3,6 +3,7 @@
 namespace rkphplib;
 
 require_once(__DIR__.'/Exception.class.php');
+require_once(__DIR__.'/ADatabase.class.php');
 
 use rkphplib\Exception;
 
@@ -59,21 +60,29 @@ public static function create($dsn) {
  *
  * @throws rkphplib\Exception
  * @param string $dsn 
+ * @param map $query_map (default = null)
  * @return ADatabase
  */
-public static function getInstance($dsn) {
+public static function getInstance($dsn, $query_map = null) {
 
 	if (empty($dsn)) {
 		throw new Exception('empty dsn');
 	}
 
+	$db_id = ADatabase::computeId($dsn, $query_map);
+
 	for ($i = 0; $i < count(self::$pool); $i++) {
-		if (self::$pool[$i]->getDSN() == $dsn && !self::$pool[$i]->hasResultSet()) {
+		if (self::$pool[$i]->getId() == $db_id && !self::$pool[$i]->hasResultSet()) {
 			return self::$pool[$i];
 		}
 	}
 
 	array_push(self::$pool, self::create($dsn));
+
+	if (!is_null($query_map)) {
+		self::$pool[$i]->setQueryHash($query_map);
+	}
+
 	return self::$pool[$i];
 }
 
