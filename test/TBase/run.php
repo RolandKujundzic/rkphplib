e32450dfc4759c0395f92eee3ad54b539dd0dcc8
Roland Kujundzic <roland@inkoeln.com>
2017-01-14 17:01:24 +0100

    log_debug added

--- test/TBase/run.php
+++ test/TBase/run.php
@@ -3,6 +3,7 @@
 global $th;
 
 if (!isset($th)) {
+	require_once(dirname(dirname(__DIR__)).'/src/lib/log_debug.php');
 	require_once(dirname(dirname(__DIR__)).'/src/TestHelper.class.php');
 	$th = new rkphplib\TestHelper();
 }
