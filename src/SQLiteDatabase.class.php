fb9913b93bdb88970c8b7915f7fd3dc533882214
Roland Kujundzic <roland@inkoeln.com>
2017-01-29 14:21:46 +0100

    no result error message

--- src/SQLiteDatabase.class.php
+++ src/SQLiteDatabase.class.php
@@ -177,8 +177,10 @@ public function selectHash($query, $key_col = 'name', $value_col = 'value', $ign
 
 
 /**
- * Execute table and return result.
+ * Execute table and return result. If res_count > 0 and result is empty
+ * throw "no result" error message.
  *
+ * @throws
  * @param string $query
  * @param int $res_count
  * @return table
@@ -218,7 +220,12 @@ private function _fetch($query, $rbind = null, $rcount = 0) {
 	$res = array();
 
 	if ($rcount > 0 && $rnum != $rcount) {
-		throw new Exception('unexpected number of rows', $rnum.' != '.$rcount);
+		if ($rnum == 0) {
+			throw new Exception('no result', $rcount.' rows expected');
+		}
+		else {
+			throw new Exception('unexpected number of rows', $rnum.' != '.$rcount);
+		}
 	}
 
 	if ($rcount < 0 && -1 * $rcount > $rnum) {
@@ -358,7 +365,12 @@ private function _fetch_stmt($stmt, $rbind = null, $rcount = 0) {
 	$res = array();
 
 	if ($rcount > 0 && $rnum != $rcount) {
-		throw new Exception('unexpected number of rows', $rnum.' != '.$rcount);
+		if ($rnum == 0) {
+			throw new Exception('no result', $rcount.' rows expected');
+		}
+		else {
+			throw new Exception('unexpected number of rows', $rnum.' != '.$rcount);
+		}
 	}
 
 	if ($rcount < 0 && -1 * $rcount > $rnum) {
