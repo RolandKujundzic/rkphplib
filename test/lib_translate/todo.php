7b363dbc892cae5a8f752c6fd0947442c7ac7a1a
Roland Kujundzic <roland@inkoeln.com>
2017-02-12 02:48:12 +0100

    refactoring

---
+++ test/lib_translate/todo.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(dirname(dirname(__DIR__)).'/src/lib/translate.php');
+
+function a() { rkphplib\lib\translate('error_a'); }
+
+class B {
+	public function __construct() { $this->_x(); }
+	private function _x() { rkphplib\lib\translate('error_B::_x()'); }
+}
+
+a();
+$b = new B();
+
