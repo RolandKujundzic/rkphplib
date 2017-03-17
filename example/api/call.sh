fa4f6764e9f74284fcb294f39c7f118bddacda11
Roland Kujundzic <roland@inkoeln.com>
2017-03-17 08:24:17 +0100

    multipart/form-data calls

--- example/api/call.sh
+++ example/api/call.sh
@@ -2,4 +2,6 @@
 
 # curl -G -H "Accept: application/json" "http://test:test@localhost:10080/some/action/?a=hallo&x=12"
 # curl -G --data "a=yuck&x=5" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json" "http://test:test@localhost:10080/some/action/?a=hallo&x=12"
-curl --data "a=hallo&x=12" -H "Accept: application/xml" "http://test:test@localhost:10080/signup/user/en_EN?x=3&z=5"
+# curl --data "a=hallo&x=12" -H "Accept: application/xml" "http://test:test@localhost:10080/signup/user/en_EN?x=3&z=5"
+curl --form "a=15" --form "b=just testing" --form "fileupload=@call.sh" "http://test:test@localhost:10080/file/upload"
+# curl -X PUT --form "fileupload=@call.sh" "http://test:test@localhost:10080/file/upload"
