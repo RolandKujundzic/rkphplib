bed069da9a41e070fc21a15917f5b53f8518b5a6
Roland Kujundzic <roland@inkoeln.com>
2017-03-17 03:36:15 +0100

    new calls added

--- example/api/call.php
+++ example/api/call.php
@@ -7,24 +7,53 @@ require_once(PATH_RKPHPLIB.'APICall.class.php');
 use \rkphplib\APICall;
 
 
-$data = [ 'x' => 18, 'y' => 'yalla' ];
+/**
+ *
+ */
+function apicall($conf, $data) {
+	$api_default = [ 
+		'url' => 'http://localhost:10080',
+		'auth' => 'basic_auth',
+		'token' => 'test:test',
+	];
 
-$api = new APICall();
 
-$api->set('url', 'http://localhost:10080');
-$api->set('uri', '/some/path');
-$api->set('method', 'get');
-$api->set('auth', 'basic_auth');
-$api->set('token', 'test:test');
-$api->set('content', 'application/json');
-$api->set('accept', 'application/xml');
+	$api = new APICall(array_merge($api_default, $conf));
 
-if ($api->exec($data)) {
-	print "\nAPICall succeded\n";
+	if ($api->exec($data)) {
+		print "\nAPICall succeded\n";
+	}
+	else {
+		print "\nAPICall failed\n";
+	}
+
+	print "INFO: ".print_r($api->info, true)."\n";
+	print "RESULT: ".print_r($api->result, true)."\n";
 }
-else {
-	print "\nAPICall failed\n";
-}
 
-print "INFO: ".print_r($api->info, true)."\n";
-print "RESULT: ".print_r($api->result, true)."\n";
+
+/*
+apicall([
+	'uri' => '/some/action',
+	'method' => 'get',
+	'content' => '',
+	'accept' => ''
+], [ 'x' => 18, 'y' => 'yalla' ]);
+*/
+
+/*
+apicall([
+	'uri' => 'some/action/?x=5&y=hello&z=hohoho',
+	'method' => 'get',
+	'content' => 'application/x-www-form-urlencoded',
+	'accept' => 'text/plain'
+], [ 'x' => 18, 'y' => 'yalla' ]);
+*/
+
+apicall([
+	'uri' => '/signup/user/de?x=5&y=hello',
+	'method' => 'post',
+	'content' => 'application/x-www-form-urlencoded',
+	'accept' => 'application/json'
+], [ 'x' => 18, 'z' => 'yalla' ]);
+
