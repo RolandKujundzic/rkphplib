759b22308dfc1085eeb168ff7566213bd9786407
Roland Kujundzic <roland@inkoeln.com>
2017-03-18 04:21:15 +0100

    API extracted to ApiExample.class.php

--- example/api/index.php
+++ example/api/index.php
@@ -2,60 +2,10 @@
 
 define('PATH_RKPHPLIB', __DIR__.'/../../src/');
 
-require_once(PATH_RKPHPLIB.'ARestAPI.class.php');
-
-use \rkphplib\RestServerException;
-use \rkphplib\ARestAPI;
-use \rkphplib\File;
+require_once('ApiExample.class.php');
 
 
-/**
- * Simple REST Server example.
- * 
- * @author Roland Kujundzic
- */
-class APIExample extends ARestAPI {
-
-  public function checkRequest() {
-	  if (empty($this->request['token']) || $this->request['token'] != 'test:test') { 
-			throw new RestServerException('invalid api token', self::ERR_INVALID_INPUT, 400);
-		}
-
-		return [];
-  }
-
-	// PUT: multipart/form-data fileupload
-	protected function putFileUpload() {
-    return $this->request;
-	}
-
-	// POST: multipart/form-data fileupload
-	protected function postFileUpload() {
-    return $this->request;
-	}
-
-	// POST: signup/:user_type/:locale - two parameter
-	protected function postSignup($user_type, $locale) {
-    return $this->request;
-	}
-
-	// PUT: something/:id - one parameter
-	protected function putSomething($id) {
-    return $this->request;
-	}
-
-	// GET: some/action - no parameter
-  protected function getSomeAction() {
-   	return $this->request;
-  }
-}
-
-
-/**
- * M A I N 
- */
-
-$api = new APIExample([ 'log_dir' => '/tmp/api' ]);
+$api = new ApiExample([ 'log_dir' => '/tmp/api' ]);
 
 if (!empty($_SERVER['argv'][0])) {
 	require_once(PATH_RKPHPLIB.'File.class.php');
@@ -65,11 +15,11 @@ if (!empty($_SERVER['argv'][0])) {
 	$index_php = $_SERVER['argv'][0];
 
 	if (!File::exists(__DIR__.'.htaccess')) {
-		File::save(__DIR__.'/.htaccess', APIExample::apacheHtaccess());
+		File::save(__DIR__.'/.htaccess', ApiExample::apacheHtaccess());
 	}
 
 	if (!File::exists(__DIR__.'/routing.php')) {
-		File::save(__DIR__.'/routing.php', APIExample::phpAPIServer());
+		File::save(__DIR__.'/routing.php', ApiExample::phpAPIServer());
 	}
 
 	print "\nApache2: Use ".dirname(__DIR__)." as document root\n";
