7b8e07e4f9599e850554db0734b1220207b28171
Roland Kujundzic <roland@inkoeln.com>
2017-03-18 04:21:35 +0100

    update

--- example/api/call.sh
+++ example/api/call.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+#
+# uncomment "curl ... " api call
+#
 
-# curl -G -H "Accept: application/json" "http://test:test@localhost:10080/some/action/?a=hallo&x=12"
-# curl -G --data "a=yuck&x=5" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json" "http://test:test@localhost:10080/some/action/?a=hallo&x=12"
-# curl --data "a=hallo&x=12" -H "Accept: application/xml" "http://test:test@localhost:10080/signup/user/en_EN?x=3&z=5"
-curl --form "a=15" --form "b=just testing" --form "fileupload=@call.sh" "http://test:test@localhost:10080/file/upload"
-# curl -X PUT --form "fileupload=@call.sh" "http://test:test@localhost:10080/file/upload"
+## test invalid route
+# curl -G -H "Accept: application/json" "http://test:test@localhost:10080/some/action/?a=hello&x=12"
+
+## valid GET route - data is appended to query (overwrite)
+# curl -G --data "a=yuck&x=5" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json" "http://test:test@localhost:10080/user/388?a=hello&x=12"
+
+## valid POST route - post is prefered to get - output is xml 
+# curl --data "a=hello&x=12" -H "Accept: application/xml" "http://test:test@localhost:10080/user/manager/en?x=3&z=5"
+
+## change user 1 data
+# curl -X PUT --data '{"firstname":"John","lastname":"Doe"}' -H "Content-type: application/json" "http://test:test@localhost:10080/user/1"
+
+## get user without id
+# curl "http://test:test@localhost:10080/user"
+
+## get user 17
+# curl "http://test:test@localhost:10080/user/17"
+
+## delete user file 132
+# curl -X DELETE "http://test:test@localhost:10080/user/file/132"
+
+## post file
+# curl --form "uid=15" --form "upload_type=logo" --form "upload=@call.sh" "http://test:test@localhost:10080/user/file"
+
+## change user file
+curl -X PUT --form "fileupload=@call.sh" "http://test:test@localhost:10080/user/file/4"
