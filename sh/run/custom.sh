439646c3fb4128f854998ffbac91c42d618d2a7c
Roland Kujundzic <roland@inkoeln.com>
2017-04-27 11:48:52 +0200

    initial

---
+++ sh/run/custom.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+function _docs() {
+	# create apigen documentation
+	test -d docs/api && rm -rf docs/api
+	vendor/apigen/apigen/bin/apigen generate -s ./src -d ./docs/api
+}
+
+
+#------------------------------------------------------------------------------
+function _test() {
+	# run all tests
+	php test/run.php
+}
+
+
+#------------------------------------------------------------------------------
+function _ubuntu() {
+	test -f /usr/bin/apt-get || _abort "apt-get not found"
+	echo "Install php + mysql + nginx"
+	sudo apt-get -y update && sudo apt-get -y install php5-cli php5-sqlite php5-curl php5-gd php5-mcrypt php5-xdebug \
+		php5-fpm mysql-server mysql-client php5-mysql nginx && sudo php5enmod mcrypt
+
+	if ! test -f /etc/nginx/sites-available/default.original; then
+		local SITE=/etc/nginx/sites-available/default
+		echo "Overwrite $SITE (save previous version as default.original)"
+		cp $SITE /etc/nginx/sites-available/default.original
+		echo 'server {' > $SITE
+		echo 'listen 80 default_server; root /var/www/html; index index.html index.htm index.php; server_name localhost;' >> $SITE
+		echo 'location / { try_files $uri $uri/ =404; }' >> $SITE
+		echo 'location ~ \.php$ { fastcgi_pass unix:/var/run/php5-fpm.sock; fastcgi_index index.php; include fastcgi_params; }' >> $SITE
+		echo '}' >> $SITE
+	fi
+
+	echo "start php5-fpm + nginx + mysql"
+	service php5-fpm restart
+	service nginx restart
+	service mysql restart
+}
+
+
+#------------------------------------------------------------------------------
+function _docker_osx {
+	echo -e "\nStart docker-machine default\n"
+	docker-machine start default
+
+	echo -e "\nSet docker env and restart rkphplib:\n"	
+	echo 'eval $(docker-machine env default)'
+	echo 'docker stop rkphplib; docker rm rkphplib'
+	echo 'docker run -it -v $PWD:/var/www/html/rkphplib -p 80:80 --name rkphplib rolandkujundzic/ubuntu_trusty_dev bash'
+	echo
+}
+
+
---
+++ sh/run/main.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+case $1 in
+composer)
+	_composer $2
+	;;
+test)
+	_test
+	;;
+docs)
+	_apigen_doc
+	;;
+mb_check)
+	_mb_check
+	;;
+ubuntu)
+	_ubuntu
+	;;
+docker_osx)
+	_docker_osx
+	;;
+*)
+	echo -e "\nSYNTAX: $0 [composer|docs|test|mb_check|ubuntu|docker_osx]\n"
+	exit 1
+esac
+
