594c056db22b6e744b40bcfd58964ef1359ec7b8
Roland Kujundzic <roland@inkoeln.com>
2017-06-24 12:15:09 +0200

    key-value split added

--- src/lib/split_table.php
+++ src/lib/split_table.php
@@ -2,25 +2,26 @@
 
 namespace rkphplib\lib;
 
+require_once(__DIR__.'/../Exception.class.php');
 require_once(__DIR__.'/split_str.php');
+
+use \rkphplib\Exception;
 
 
 /**
- * Split table string at delimiter. Ignore empty rows. Trim all cells. Escape with leading backslash.
- * Default delimiter: col = "|",  row = "\n"
+ * Split table string at delimiter. Ignore empty rows. Trim all cells. 
+ * If $split_cell is set split into key value. Escape delimiter with 
+ * leading backslash. Default delimiter: col = "|",  row = "\n"
  *
  * @param string $table_str
  * @param string $col_delimiter
  * @param string $row_delimiter
- * @param boolean $trim_cell
+ * @param string $split_cell
  * @return array
  */
-function split_table($table_str, $col_delimiter = '|', $row_delimiter = "\n", $trim_cell = true) {
+function split_table($table_str, $col_delimiter = '|', $row_delimiter = "\n", $split_cell = '') {
   $table = [];
-
-	if ($trim_cell) {
-		$table_str = trim($table_str);
-	}
+	$table_str = trim($table_str);
 
 	if (strlen($table_str) == 0) {
 		return $table;
@@ -30,7 +31,27 @@ function split_table($table_str, $col_delimiter = '|', $row_delimiter = "\n", $t
 
 	for ($i = 0; $i < count($rows); $i++) {
 		if (strlen($rows[$i]) > 0) {
-			array_push($table, split_str($col_delimiter, $rows[$i]));
+			if ($split_cell) {
+				$v = split_str($col_delimiter, $rows[$i]);
+				$scl = mb_strlen($split_cell);
+				$table_row = [];
+
+				for ($j = 0; $j < count($v); $j++) {
+					if (($pos = mb_strpos($v[$j], $split_cell)) > 0) {
+						$key = trim(mb_substr($v[$j], 0, $pos));
+						$value = trim(mb_substr($v[$j], $pos + $scl));
+						$table_row[$key] = $value;
+					}
+					else {
+						throw new Exception('split_cell failed in '.$v[$j], "i=$i j=$j row=".$rows[$i]);
+					}
+				}
+			}
+			else {
+				$table_row = split_str($col_delimiter, $rows[$i]);
+			}
+
+			array_push($table, $table_row);
 		}
 	}
 
