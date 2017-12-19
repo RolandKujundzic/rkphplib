2d0a38645061820600beed9792e266c76b5ab141
Roland Kujundzic <roland@inkoeln.com>
2017-12-19 15:21:53 +0100

    initial

---
+++ src/StringHelper.class.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace rkphplib;
+
+
+/**
+ * String manipulation.
+ *
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ */
+class StringHelper {
+
+/**
+ * Remove all whitespace from html. Example:
+ *
+ * [ <a href="...">Home</a>  |  <a href="..."> Somewhere </a> ] = [<a href="...">Home</a>|<a href="...">Somewhere</a>]
+ *
+ * @param string $html
+ * @return string
+ */
+public static function removeHtmlWhiteSpace($html) {
+	return preg_replace('/\s+(<)|(>)\s+/', '\2\1', $html);
+}
+
+
+/**
+ * Remove all attributes from all html tags. Example:
+ * 
+ * <a href="..." class="..."><i class="..."></i> Text</a> = <a><i></i> Text</a>
+ *
+ * @param string $html
+ * @return string
+ */
+public static function removeHtmlAttributes($html) {
+	return preg_replace('/<([a-zA-Z]+).*?>/', '<\1>', $html);
+}
+
+
+}
+
