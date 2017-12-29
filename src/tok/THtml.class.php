94fc59cca7f0f87036de0905be41b48e23970542
Roland Kujundzic <roland@inkoeln.com>
2017-12-29 16:57:36 +0100

    refactoring

--- src/tok/THtml.class.php
+++ src/tok/THtml.class.php
@@ -15,34 +15,54 @@ use \rkphplib\Exception;
  */
 class THtml implements TokPlugin {
 
+
 /**
- *
+ * 
  */
 public function getPlugins($tok) {
   $plugin = [];
-  $plugin['innerHtml'] = TokPlugin::POSTPROCESS;
+  $plugin['html:inner'] = TokPlugin::REQUIRE_PARAM | TokPlugin::REQUIRE_BODY | TokPlugin::POSTPROCESS;
+	$plugin['html:tidy'] = TokPlugin::NO_PARAM | TokPlugin::NO_BODY | TokPlugin::POSTPROCESS;
+	$plugin['html'] = 0;
   return $plugin;
+}
+
+
+/**
+ * Postprocess output. Pretty print Html via DOM.
+ *
+ * @param string $html
+ * @return string
+ */
+public function tok_html_tidy($html) {
+	$dom = new \DOMDocument();
+
+	$dom->preserveWhiteSpace = false;
+	$dom->formatOutput = true;
+	$dom->loadHTML($html);
+
+	return $dom->saveHTML();
 }
 
 
 /**
  * Postprocess output. Replace inner html of tag (= <tag>).
  *
- * @tok {innerHtml:title}Replace <title> with this{:innerHtml}
+ * @tok {innerHtml:title}Replace "title" with this{:innerHtml}
  *
  * @see DateCalc::formatDateStr()
- * @param string $param
- * @param string $arg
+ * @param string $tag
+ * @param string $innerHtml
  * @param string $html
  * @return string
  */
-public function tok_innerHtml($tag, $arg, $html) {
+public function tok_html_inner($tag, $innerHtml, $html) {
 	$start = mb_stripos($html, '<'.$tag.'>');
 	$tag_len = mb_strlen($tag) + 2;
 	$end = mb_stripos($html, '</'.$tag.'>', $start + $tag_len);
 
 	if ($start > 0 && $end >= $start + $tag_len) {
-		$res = mb_substr($html, 0, $start).'<'.$tag.'>'.$arg.'</'.$tag.'>'.mb_substr($html, $end + $tag_len + 1);
+		$res = mb_substr($html, 0, $start).'<'.$tag.'>'.$innerHtml.'</'.$tag.'>'.mb_substr($html, $end + $tag_len + 1);
 	}
 
 	return $res;
