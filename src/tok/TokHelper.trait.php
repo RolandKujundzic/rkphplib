da126ace2d4c3ee18e2def57588c8d7890379b24
Roland Kujundzic <roland@kujundzic.de>
2018-01-28 15:02:46 +0100

    getMapKeys added

--- src/tok/TokHelper.trait.php
+++ src/tok/TokHelper.trait.php
@@ -2,6 +2,10 @@
 
 namespace rkphplib\tok;
 
+require_once(dirname(__DIR__).'/Exception.class.php');
+
+use \rkphplib\Exception;
+
 
 /**
  * Trait collection for Tokenizer plugins.
@@ -47,6 +51,74 @@ private function checkMap($plugin_param, $map, $required_keys) {
 			$example = $this->tok->getPluginTxt($plugin_param, "$key=...");
 			throw new Exception("missing parameter $key (use $example)");
 		}
+	}
+}
+
+
+/**
+ * Return submap or value. It does not matter if map is multimap or multimap keys
+ * are used. 
+ *
+ * @code
+ * $x = [ 'a' => 7, 'a.b.0' => 18, 'a.b.1' => 19, 'c' => [ 0 => 5, 1 => 6 ], 'd' => [ 'x' => 3, 'y' => 4 ] ];
+ * getMapKeys('a', $x) == 7; getMapKeys('a.b') == [ 18, 19 ]; getMapKeys('c') == [ 5, 6 ];
+ * getMapKeys('c.0', $x) == 5; getMapKeys('c.x', $x) == 3; getMapKeys('d') == [ 'x' => 3, 'y' => 4 ]
+ * @:
+ *
+ * @throws
+ * @param string $path_str e.g. name1.name2
+ * @param map $map
+ * @return map|string|false
+ */
+private function getMapKeys($path_str, $map) {
+
+	if (empty($path_str)) {
+		throw new Exception('empty path', 'map: '.print_r($map, true));
+	}
+
+	$path = explode('.', $path_str);
+	$is_array = true;
+	$found = true; 
+	$pkey = '';
+
+	// assume multi-map
+	while (count($path) > 0) {
+		$pkey = array_shift($path);
+
+		if (isset($map[$pkey]) || array_key_exists($pkey, $map)) {
+			if (is_array($map[$pkey])) {
+				$map = $map[$pkey];
+			}
+			else {
+				$is_array = false;
+			}
+		}
+		else {
+			$found = false;
+			break;
+		}
+	}
+
+	if (!$found) {
+		// check if we are using multi-map-keys
+		$len = mb_strlen($path_str); 
+		$res = [];
+
+		foreach ($map as $mkey => $value) {
+			if (mb_strpos($mkey, $path_str) === 0) {
+				if ($mkey == $path_str) {
+					return $value;
+				}
+				
+				$key = mb_substr($mkey, $len + 1);
+				$res[$key] = $value;
+			}
+		}
+
+		return (count($res) > 0) ? $res : false;
+	}
+	else {
+		return $is_array ? $map : $map[$pkey]; 
 	}
 }
 
