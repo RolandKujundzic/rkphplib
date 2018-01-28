3a791d2f90b3e8b28154106f7df33f4c20a210c4
Roland Kujundzic <roland@kujundzic.de>
2018-01-28 15:03:28 +0100

    TokHelper test added

--- test/tok_TokHelper/run.php
+++ test/tok_TokHelper/run.php
@@ -1,16 +1,66 @@
 <?php
 
-require_once('src/tok/TokHelper.trait.php');
+global $th;
+
+if (!isset($th)) {
+  require_once(dirname(dirname(__DIR__)).'/src/TestHelper.class.php');
+  $th = new rkphplib\TestHelper();
+}
+
+$th->load('src/tok/TokHelper.trait.php');
+$th->load('src/File.class.php');
 
 
-class X {
+/**
+ *
+ */
+class MapTest {
 	use \rkphplib\tok\TokHelper;
 
-	public function get($key, $map) {
-		return $this->getMapKeysLike($key, $map);
+	private $map = null;
+	private $result = null;
+
+	public function useMap($map) {
+		$this->result = [];
+		$this->map = $map;
+		return $this;
+	}
+
+	public function getResult() {
+		return $this->result;
+	}
+
+	public function call($key) {
+		$value = $this->getMapKeys($key, $this->map);
+		array_push($this->result, [ $key, $value ]);
+		return $this;
+	}
+
+	public function cmp($key, $result) {
+		$value = $this->getMapKeys($key, $this->map);
+	
+		$value_out = print_r($value, true);
+		$result_out = print_r($result, true);
+
+		if ($value_out != $result_out) {
+			throw new Exception("ERROR: $key: $value_out != $result_out");
+		}
+		else {
+			print " . ";
+		}
+
+		return $this;
+	}
+
+	public function ok() {
+		print "ok\n";
 	}
 }
 
+
+/*
+ * M A I N
+ */
 
 $a = [
 	'm' => 7,
@@ -18,6 +68,7 @@ $a = [
 	'A' => [
 		'X' => [
 			'm' => 17,
+			'n' => 18,
 			],
 		'Y' => 9,
 		'Z' => [
@@ -26,16 +77,40 @@ $a = [
 		]
 	];
 
-$x = new X();
+$b = [
+	'm' => 7,
+	'v' => 8,
+	'A.X.m' => 17,
+	'A.X.n' => 18,
+	'A.Y' => 9,
+	'A.Z.m' => 27,
+	];
 
-print 'a[v]='.print_r($x->get('v', $a), true)."\n";
-print 'a[A.Y]='.print_r($x->get('A.Y', $a), true)."\n";
-exit(0);
 
-print 'a[A.X.m]='.print_r($x->get('A.X.m', $a), true)."\n";
-print 'a[A.Z.m]='.print_r($x->get('A.Z.m', $a), true)."\n";
-print 'a[A]='.print_r($x->get('A', $a), true)."\n";
-print 'a[A.Y]='.print_r($x->get('A.Y', $a), true)."\n";
-print 'a[A.Z]='.print_r($x->get('A.Z', $a), true)."\n";
-print 'a[A.X.*]='.print_r($x->get('A.X.*', $a), true)."\n";
+$test = new MapTest();
+
+// doc example
+/*
+$x = [ 'a' => 7, 'a.b.0' => 18, 'a.b.1' => 19, 'c' => [ '1' => 5, '2' => 6 ], 'd' => [ 'x' => 3, 'y' => 4 ] ];
+$test->useMap($x)->cmp('a', 7)->cmp('a.b', [ 18, 19 ])->cmp('c', [ 1 => 5, 2 => 6 ])->cmp('c.1', 5)->cmp('d.x', 3)
+	->cmp('d', [ 'x' => 3, 'y' => 4 ])->ok();
+*/
+
+$a_out = $test->useMap($a)->call('v')->call('A.Y')->call('A.X.m')->call('A.X.n')->call('A.Z.m')->getResult();
+$b_out = $test->useMap($b)->call('v')->call('A.Y')->call('A.X.m')->call('A.X.n')->call('A.Z.m')->getResult();
+$th->compare('string result: ', [ $a_out ], [ $b_out ]);
+
+$a_out = $test->useMap($a)->call('h')->call('A.t')->getResult();
+$b_out = $test->useMap($b)->call('h')->call('A.t')->getResult();
+$th->compare('no result: ', [ $a_out ], [ $b_out ]);
+
+$a_out = $test->useMap($a, 'A')->call('A')->call('A.X')->call('A.Y')->call('A.Z')->getResult();
+// \rkphplib\File::serialize('t3.ok', $a_out);
+$a_ok = \rkphplib\File::unserialize('t3.ok');
+$th->compare('submap a result: ', [ $a_out ], [ $a_ok ]);
+
+$b_out = $test->useMap($b, 'A')->call('A')->call('A.X')->call('A.Y')->call('A.Z')->getResult();
+// \rkphplib\File::serialize('t4.ok', $b_out);
+$b_ok = \rkphplib\File::unserialize('t4.ok');
+$th->compare('submap b result: ', [ $b_out ], [ $b_ok ]);
 
