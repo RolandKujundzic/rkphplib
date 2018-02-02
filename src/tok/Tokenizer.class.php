7e2e322b84b7e0021e6439809bdef34e326376b6
Roland Kujundzic <roland@inkoeln.com>
2018-02-02 12:52:02 +0100

    log_debug off

--- src/tok/Tokenizer.class.php
+++ src/tok/Tokenizer.class.php
@@ -786,7 +786,7 @@ public function callPlugin($name, $func, $args = []) {
 		throw new Exception("no such plugin method $name.".$func);
 	}
 
-	\rkphplib\lib\log_debug("Tokenizer.callPlugin($name, $func> args: ".print_r($args, true));
+	// \rkphplib\lib\log_debug("Tokenizer.callPlugin($name, $func> args: ".print_r($args, true));
 
 	if (count($args) == 0) {
 		$res = call_user_func(array($this->_plugin[$name][0], $func));
