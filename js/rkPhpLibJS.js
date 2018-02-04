6e6bb59ffde1fd7917adb1189002e9b33322dcfc
Roland Kujundzic <roland@kujundzic.de>
2018-02-04 12:01:21 +0100

    update

--- js/rkPhpLibJS.js
+++ js/rkPhpLibJS.js
@@ -1,33 +1,40 @@
 /**
  * Javascript functions referenced from rkphplib.
  *
- * @code
- * <script>
- * var rkphplib = new rkPhpLibJS();
- * </script>
- * @:
- *
  * @copyright Roland Kujundzic 2017
  * @author Roland Kujundzic <roland@kujundzic.de>
  */
-function rkPhpLibJS(options) {
+function rkPhpLibJS() {
 'use strict';
 
 /** @var this me */
 var me = this;
 
+/** @var map env */
+var env = [];
+
+
 
 /**
- * Return unix timestamp of datum in seconds. 
- * Required by TOutput.
+ * Submit element parent form. Delay submit by 1.2sec. Reset submit if new
+ * search parameter is added within delay.
  *
- * @for TOutput
+ * @plugin TOutput.tok_search
  * @param Element el
  */
 this.searchOutput = function(el) {
-	
+	var value = el.value;
+
+	if (env.search_output_timeout) {
+		clearTimeout(env.search_output_timeout);
+		env.search_output_timeout = null;
+	}
+
+	env.search_output_timeout = setTimeout(function() { el.form.submit(); }, 1200);
 };
 
 
 }
 
+
+rkphplib = new rkPhpLibJS();
