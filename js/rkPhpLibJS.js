bc7943b59eac019930cda0cd0b931562a5e1f72c
Roland Kujundzic <roland@kujundzic.de>
2018-02-04 11:30:46 +0100

    initial

---
+++ js/rkPhpLibJS.js
@@ -0,0 +1,33 @@
+/**
+ * Javascript functions referenced from rkphplib.
+ *
+ * @code
+ * <script>
+ * var rkphplib = new rkPhpLibJS();
+ * </script>
+ * @:
+ *
+ * @copyright Roland Kujundzic 2017
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ */
+function rkPhpLibJS(options) {
+'use strict';
+
+/** @var this me */
+var me = this;
+
+
+/**
+ * Return unix timestamp of datum in seconds. 
+ * Required by TOutput.
+ *
+ * @for TOutput
+ * @param Element el
+ */
+this.searchOutput = function(el) {
+	
+};
+
+
+}
+
