a72a628867d88c7f97f77483dfbb2ea573f1db5a
Roland Kujundzic <roland@inkoeln.com>
2018-02-11 18:17:10 +0100

    ajax() added

--- js/rkPhpLib.js
+++ js/rkPhpLib.js
@@ -8,7 +8,7 @@
  * @copyright Roland Kujundzic 2017
  * @author Roland Kujundzic <roland@kujundzic.de>
  */
-function rkPhpLibJS() {
+function rkPhpLib() {
 
 /** @var this me */
 var me = this;
@@ -16,6 +16,60 @@ var me = this;
 /** @var map env */
 var env = [];
 
+
+
+/**
+ * Execute ajax call. Options:
+ *
+ * - method: GET (=default)
+ * - url: required
+ * - json: true|false (JSON.parse result, default = false)
+ * - success: callback success(data) if sucess
+ * - error: callback error(status, data) if server error or error(0, '') if connection error - if not set throw exception
+ *
+ * @param map options
+ */
+this.ajax = function(options) {
+	var request = new XMLHttpRequest();
+
+	if (!options.method) {
+		options.method = 'GET';
+	}
+
+	if (!options.error) {
+		options.error = function(code, data) {
+			throw 'ajax query failed with code ' + code;
+		};
+	}
+
+	request.open(options.method, options.url, true);
+
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			var data = request.responseText;
+
+			if (data.indexOf('html') > -1) {
+    		options.error(request.status, request.responseText);
+			}
+			else {
+				if (options.json) {
+					data = JSON.parse(data);
+				}
+
+				options.success(data);
+			}
+		}
+		else {
+    	options.error(request.status, request.responseText);
+		}
+	};
+
+	request.onerror = function() {
+  	options.error(0, '');
+	};
+
+	request.send();
+};
 
 
 /**
@@ -51,27 +105,19 @@ this.search = function(el) {
 	}
   
 	if ((name = el.getAttribute('name')) && (url = el.getAttribute('data-search_url'))) {
-		window.location.href = url + '&' + name = encodeURI(search);
+		window.location.href = url + '&' + name + '=' + encodeURI(search);
 	}
 };
 
 
 /**
- * Use input el and datalist el.getAttribute('list'). Append search parameter to 
- * el.getAttribute(data-query). Create new datalist from ajax result { "key": "value", .... }.
- * Example:
- * 
- * <input type="text" id="search" name="search" autocomplete="off" onblur="rkphplib.search(this)" 
- *   onkeydown="if (event.keyCode == 13) rkphplib.search(this)"
- *   list="search_list" data-url="index.php?dir=shop/search&search=" 
- *   data-query="index.php?dir=ajax/search&search=" 
- *   data-last="" placeholder="search" />
- * <datalist id="search_list"></datalist>
+ * Update search list.
  *
- * @see example/ajax_live_search 
+ * @see example/ajax_live_search
+ * @param Element el
  */
-function updateSearchList() {
-	var last_search = $('#search').attr('data-last'), curr_search = $('#search').val();
+this.updateSearchList = function(el) {
+	var last_search = el.getAttribute('data-search'), curr_search = el.value;
 
 	if (!curr_search || curr_search === last_search) {
 		return;
@@ -83,36 +129,37 @@ function updateSearchList() {
 	}
 
 	env.update_search_timeout = setTimeout(function() {
-		var query = $('#search').attr('data-query');
+		var list_url = el.getAttribute('data-search_list_url') + '&' + el.getAttribute('name') + '=';
 
-		if (!query) {
-			query = 'index.php?dir=ajax/search';
-		}
-
-		$.get(query + '&search=' + encodeURI(curr_search), function (data) {
-			var key, list = JSON.parse(data), search_list = $('#search_list');
+		me.ajax({ url: list_url + encodeURI(curr_search), json: true, success: function (list) {
+			var key, options = '', search_list = document.getElementById(el.getAttribute('list'));
 
 			if (navigator.userAgent.indexOf("Safari") > -1) {
 				// Safari has no datalist support
-				search_list.html('<select onChange="search(this)"></select>');
-				search_list = $('#search_list > select');
+				search_list.innerHTML = '<select onChange="rkphplib.search(this)"></select>';
+				var sl_id = '#' + el.getAttribute('list');
+				search_list = document.querySelector(sl_id + ' > select');
 			}
-			else {
-				search_list.html('');
-	    }
 
 			for (key in list) {
 				if (key) {
-					$('<option data-id="' + key + '">' + list[key] + '</option>').appendTo(search_list);
+					options += '<option data-id="' + key + '">' + list[key] + '</option>';
 				}
 			}
 
-			$('#search').attr('data-last', curr_search);
-		});
+			search_list.innerHTML = options;
+
+			el.setAttribute('data-search', curr_search);
+		}});
 	}, 800);
-}
+};
 
 
+/**
+ * Initialize search input. Attribute name and data-search_list_url required.
+ * 
+ * @param Element el
+ */
 function initLiveSearch(el) {
 	var name, id, list, datalist;
 
@@ -122,10 +169,15 @@ function initLiveSearch(el) {
 
 	if (!(id = el.getAttribute('id'))) {
 		if (document.getElementById(name)) {
-			throw 'Search input has no id';
+			throw 'Search input ' + name + ' has no id';
 		}
  
-		el.setAttribute('id', name);
+		id = name;
+		el.setAttribute('id', id);
+	}
+
+	if (!document.getElementById(id)) {
+		throw 'Search input ' + name + ' has no id';
 	}
 
 	if (!(list = el.getAttribute('list'))) {
@@ -150,8 +202,7 @@ function initLiveSearch(el) {
 document.addEventListener('DOMContentLoaded', function () { 
 	var i, list;
 
-	if ((list = document.querySelectorAll('input[data-search_query=""]'))) {
-console.log('list: ', list);
+	if ((list = document.querySelectorAll('input[data-search_list_url]'))) {
 		for (i = 0; i < list.length; i++) {
 			initLiveSearch(list[i]);
 		}
@@ -163,5 +214,5 @@ console.log('list: ', list);
 }
 
 
-var rkphplib = new rkPhpLibJS();
+var rkphplib = new rkPhpLib();
 
