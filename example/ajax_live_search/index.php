4bc33968a6e3d33422aaddc0c9a7c87107ba3033
Roland Kujundzic <roland@inkoeln.com>
2018-02-11 13:40:51 +0100

    initial

---
+++ example/ajax_live_search/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('../../src/tok/Tokenizer.class.php');
+require_once('../../src/tok/TBase.class.php');
+
+
+$tok = new \rkphplib\tok\Tokenizer();
+
+$t_base = new \rkphplib\tok\TBase();
+$tok->register($t_base);
+
+$tok->load($t_base->tok_find('layout.inc.html'));
+print $tok->toString();
+
