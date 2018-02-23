e7f69a5c96af1013f96cea02ee3ff98704beaf9a
Roland Kujundzic <roland@inkoeln.com>
2018-02-23 16:36:49 +0100

    plugin_map

--- src/tok/Tokenizer.class.php
+++ src/tok/Tokenizer.class.php
@@ -1118,7 +1118,7 @@ public function getTag($name) {
 private function tryPluginMap($name) {
 	static $map = [
 		'TArray' => [ 'array', 'array:set', 'array:get', 'array:shift', 'array:unshift', 'array:pop', 'array:push', 'array:join', 'array:length' ],
-		'TBase' => [ 'bootstrap:row', 'bootstrap', 'tf', 't', 'true', 'f', 'false', 'find', 'plugin', 'escape', 'unescape', 'encode', 'decode', 'get', 'const', 'include', 'include_if', 'view', 'clear', 'ignore', 'if', 'switch', 'keep', 'load', 'link', 'redo', 'toupper', 'tolower', 'join', 'set_default', 'redirect', 'var', 'esc' ],
+		'TBase' => [ 'bootstrap', 'bootstrap:row', 'tpl_set', 'tpl', 'tf', 't', 'true', 'f', 'false', 'find', 'plugin', 'escape', 'unescape', 'encode', 'decode', 'get', 'const', 'include', 'include_if', 'view', 'clear', 'ignore', 'if', 'switch', 'keep', 'load', 'link', 'redo', 'toupper', 'tolower', 'join', 'set_default', 'redirect', 'var', 'esc' ],
 		'TConf' => [ 'conf', 'conf:id', 'conf:var', 'conf:get', 'conf:get_path', 'conf:set', 'conf:set_path', 'conf:set_default', 'conf:append' ],
 		'TDate' => [ 'date' ],
 		'TEval' => [ 'eval:math', 'eval:logic', 'eval' ],
