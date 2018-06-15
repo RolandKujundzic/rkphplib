2bd6af0e63131a9e23d96c59df7a9d365d2e2787
Roland Kujundzic <roland@inkoeln.com>
2018-06-15 13:06:26 +0200

    update

--- src/tok/ACatchall.class.php
+++ src/tok/ACatchall.class.php
@@ -12,6 +12,7 @@ require_once(PATH_RKPHPLIB.'Dir.class.php');
 use \rkphplib\Exception;
 use \rkphplib\tok\Tokenizer;
 use \rkphplib\tok\TBase;
+use \rkphplib\FSEntry;
 use \rkphplib\File;
 use \rkphplib\Dir;
 
@@ -60,19 +61,13 @@ abstract public function processFile($file, $data);
 
 
 /**
- * Return tokenized file. Export relative directory path.
+ * Return tokenized file. Assume _REQUEST[dir] is exported.
  * 
  * @param string $file
  * @return string
  */
 public function parseFile($file) {
-	$relpath = $file;
-  if (($pos = strpos($file, '/')) !== false) {
-    $relpath = substr($file, $pos + 1);
-  }
-
-	$dir = dirname($relpath);
-	$_REQUEST['dir'] = ($dir != '.') ? $dir : '';
+	$this->log("parse $file");
 
 	$this->tok = new Tokenizer();
 	$tbase = new TBase();
@@ -89,15 +84,76 @@ public function parseFile($file) {
  * in directory. 
  *
  * @param string $directory
- * @param array $suffix_list
+ * @param array $suffix_list (default [ 'inc.html' ]
  */
-public function scan($directory, $suffix_list) {
-	$files = Dir::scanTree('src', [ '.html' ]);
+public function scan($directory, $suffix_list = [ 'inc.html' ]) {
+	$files = Dir::scanTree('src', $suffix_list);
+	$dlen = strlen($directory);
+
+	$this->log("scan $directory/");
 
 	foreach ($files as $file) {
+		$relpath = substr($file, $dlen + 1);
+		$dir = dirname($relpath);
+		$_REQUEST['dir'] = ($dir != '.') ? $dir : '';
+
 		$this->processFile($file, $this->parseFile($file));
 	}
 }
 
 
+/**
+ * Copy $source_dir content to $target_dir. Process files with suffix in
+ * $parse_suffix_list.
+ * 
+ * @param string $source_dir
+ * @param string $target_dir 
+ * @param array $parse_suffix_list (default = [ 'inc.html', 'js', 'conf' ])
+ * @param int $slen (default = 0, internal for recursive call)
+ */
+public function copy($source_dir, $target_dir, $parse_suffix_list = [ 'inc.html', 'js', 'conf' ], $slen = 0) {
+	$entries = Dir::entries($source_dir);
+
+	if (!$slen) {
+		$this->log("copy $source_dir/ to $target_dir/");
+		Dir::fixSuffixList($parse_suffix_list);
+		$slen = strlen($source_dir);
+	}
+
+	foreach ($entries as $entry) {
+		if (FSEntry::isDir($entry, false)) {
+			$this->copy($entry, $target_dir, $parse_suffix_list, $slen);
+    }
+    else if (FSEntry::isFile($entry, false)) {
+			$relpath = substr($entry, $slen + 1);
+			$target = $target_dir.'/'.$relpath;
+			$suffix = File::suffix($entry);
+
+  		Dir::create(dirname($target), 0, true);
+
+			if (in_array($suffix, $parse_suffix_list)) {
+				$dir = dirname($relpath);
+				$_REQUEST['dir'] = ($dir != '.') ? $dir : '';
+				$content = $this->processFile($target, $this->parseFile($entry));
+			}
+			else {
+				$this->log("copy $entry to $target");
+				File::copy($entry, $target);
+			}
+    }
+  }
 }
+
+
+/**
+ * Print message to stdout (prepend 2x space, append \n).
+ *
+ * @param string $message
+ */
+public function log($message) {
+	print "  $message\n";
+}
+
+
+}
+
