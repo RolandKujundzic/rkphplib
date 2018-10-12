0dc9e6ca890e0764cb97b7ed7caaff6828149d49
Roland Kujundzic <roland@kujundzic.de>
2018-10-12 13:34:19 +0200

    initial

---
+++ src/Net.class.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace rkphplib;
+
+require_once(__DIR__.'/Exception.class.php');
+
+
+/**
+ * Net access wrapper.
+ * 
+ * @author Roland Kujundzic <roland@kujundzic.de>
+ * @copyright 2018 Roland Kujundzic
+ *
+ */
+class Net {
+
+
+/**
+ * Check if remote file exists.
+ *
+ * @param string $path
+ * @return boolean
+ */
+public static function fileExists($path) {
+	return @fopen($path, 'r') == true;
+}
+
+
+}
