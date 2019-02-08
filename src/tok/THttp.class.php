31755d487732bf8cc47803d26426953e876239e5
Roland Kujundzic <roland@kujundzic.de>
2019-02-08 05:37:01 +0100

    update

--- src/tok/THttp.class.php
+++ src/tok/THttp.class.php
@@ -121,8 +121,11 @@ public static function httpGet($name) {
 			if (($pos = strpos($path, '/index.php?')) !== false || ($pos = strpos($path, '/?')) !== false) {
 				$res = $port_host.substr($path, 0, $pos);
 			}
+ 			else if (substr(getcwd().'/', -1 * strlen($path)) == $path) {
+				$res = $port_host.$path;
+			}
 			else {
-				throw new Exception('failed to detect abs_path', "port_host=[$port_host] path=[$path]");
+				throw new Exception('failed to detect abs_path', "port_host=[$port_host] path=[$path] getcwd=[".getcwd()."]");
 			}
 		}
 		else if ($name == 'host') {
