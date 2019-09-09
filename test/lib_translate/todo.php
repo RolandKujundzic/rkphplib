1a2e9a97dd18076bd10cc7ec55ec8e94982faba1
Roland Kujundzic <roland@inkoeln.com>
2019-09-09 04:40:12 +0200

    fix format

--- test/lib_translate/todo.php
+++ test/lib_translate/todo.php
@@ -2,11 +2,18 @@
 
 require_once(dirname(dirname(__DIR__)).'/src/lib/translate.php');
 
-function a() { rkphplib\lib\translate('error_a'); }
+function a() {
+	\rkphplib\lib\translate('error_a');
+}
 
 class B {
-	public function __construct() { $this->_x(); }
-	private function _x() { rkphplib\lib\translate('error_B::_x()'); }
+	public function __construct() {
+		$this->_x();
+	}
+
+	private function _x() {
+		rkphplib\lib\translate('error_B::_x()');
+	}
 }
 
 a();
