36f5bfaf545000f9a5dbfe3f5832e35c5670c0c1
Roland Kujundzic <roland@inkoeln.com>
2020-02-13 12:20:02 +0100

    update

--- src/tok/Tokenizer.class.php
+++ src/tok/Tokenizer.class.php
@@ -1219,6 +1219,7 @@ private function tryPluginMap(string $name) : void {
 		'TEval' => [ 'eval:math', 'eval:logic', 'eval' ],
 		'TFileSystem' => [ 'directory:copy', 'directory:move', 'directory:create', 'directory:exists', 'directory:entries', 'directory:is', 'directory', 'file:size', 'file:copy', 'file:exists', 'csv_file:conf', 'csv_file:append', 'csv_file:open', 'csv_file:close', 'csv_file', 'file', 'dirname', 'basename' ],
 		'TFormValidator' => [ 'fv', 'fv:init', 'fv:conf', 'fv:get', 'fv:get_conf', 'fv:check', 'fv:in', 'fv:tpl', 'fv:hidden', 'fv:preset', 'fv:error', 'fv:appendjs', 'fv:error_message', 'fv:emsg', 'fv:set_error_message' ],
+		'THighlight' => [ 'source:php', 'source:html' ],
 		'THtml' => [ 'html:tag', 'html:inner', 'html:append', 'html:meta', 'html:meta_og', 'html:tidy', 'html:xml', 'html:uglify', 'html', 'text2html', 'input:checkbox', 'input:radio', 'input', 'user_agent' ],
 		'THttp' => [ 'http:get', 'http', 'domain:idn', 'domain:utf8', 'domain' ],
 		'TJob' => [ 'job' ],
