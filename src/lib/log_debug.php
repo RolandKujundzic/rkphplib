53ed37c56100df2edae89ff8ec35900e3497b326
Roland Kujundzic <roland@inkoeln.com>
2020-03-15 20:25:05 +0100

    refactoring

--- src/lib/log_debug.php
+++ src/lib/log_debug.php
@@ -25,29 +25,49 @@ else {
   date_default_timezone_set(SETTINGS_TIMEZONE);
 }
 
-
-if (!isset($GLOBALS['OVERWRITE_SETTINGS'])) {
-	$GLOBALS['OVERWRITE_SETTINGS'] = [];
+if (!isset($GLOBALS['SETTINGS'])) {
+	$GLOBALS['SETTINGS'] = [];
 }
 
 
 /**
- * Log debug message (string or vector). If $prepend_info = true (default = false)
- * prepend timestamp and trace information. Use $GLOBALS['OVERWRITE_SETTINGS']['LOG_DEBUG'] = '...'
- * to overwrite defined SETTINGS_LOG_DEBUG.
- *
+ * Log debug message (string or vector). 
  * Disable debug log with SETTINGS_LOG_DEBUG = 0,
  * Enable logging to default error_log with SETTINGS_LOG_DEBUG = 1.
  * Enable logging to file with SETTINGS_LOG_DEBUG = 'path/debug.log'.
- * Log to STDOUT|STDERR with SETTINGS_LOG_DEBUG = 'php://STDOUT'; // 'php://STDERR', '/dev/stdout', '/dev/stderr'
- * If 'SETTINGS_LOG_DEBUG' is undefined use 'data/.log/php.log' (if data/.log exists)
- * or '/tmp/php.log'.
+ * Log to STDOUT with 'print|php://STDOUT|/dev/stdout' and to STDERR with 'php://STDERR|/dev/stderr'.
+ * If 'SETTINGS_LOG_DEBUG' is undefined use 'data/.log/php.log' (if data/.log exists) or '/tmp/php.log'.
+ * Use $GLOBALS['SETTINGS']['LOG_DEBUG'] = '...' to overwrite SETTINGS_LOG_DEBUG. 
+ * Use 2^N flag:
  *
+ * 1: prepend timestamp and trace information
+ * 2, 4, 8, 16: log level (lower = less important)
+ * 
+ * Use SETTINGS_LOG_DEBUG_LEVEL ($GLOBALS['SETTINGS']['LOG_DEBUG_LEVEL']) = 16|24|28|30 to
+ * log messages with LEVEL & LOG_DEBUG_LEVEL > 0. Use 30 to log all and 16 to log only most important.
+ * 
  * @author Roland Kujundzic <roland@kujundzic.de>
  */
-function log_debug($msg, bool $prepend_info = false) : void {
+function log_debug(string $msg, int $flag = 0) : void {
+	if (!empty($GLOBALS['SETTINGS']['LOG_DEBUG'])) {
+		$log_to = $GLOBALS['SETTINGS']['LOG_DEBUG'];
+	}
+	else if (defined('SETTINGS_LOG_DEBUG') && !empty(SETTINGS_LOG_DEBUG)) {
+		$log_to = SETTINGS_LOG_DEBUG;
+	}
+	else {
+		return;
+	}
 
-	if (!defined('SETTINGS_LOG_DEBUG') || empty(SETTINGS_LOG_DEBUG)) {
+	$log_level = 0;
+	if (!empty($GLOBALS['SETTINGS']['LOG_DEBUG_LEVEL'])) {
+		$log_level = $GLOBALS['SETTINGS']['LOG_DEBUG_LEVEL'];
+	}
+	else if (defined('SETTINGS_LOG_DEBUG_LEVEL') && !empty(SETTINGS_LOG_DEBUG_LEVEL)) {
+		$log_level = SETTINGS_LOG_DEBUG_LEVEL;
+	}
+
+	if ($log_level > 0 && ($flag & $log_level) == 0) {
 		return;
 	}
 
@@ -71,33 +91,43 @@ function log_debug($msg, bool $prepend_info = false) : void {
 		$msg = $out;
 	}
 
-	if ($prepend_info) {
+	static $last_prefix = '';
+	$prefix = '';
+
+	if ($flag & 1 == 1) {
 		list($msec, $ts) = explode(" ", microtime());
-		$log = '['.date('YmdHis', $ts).'.'.(1000 * round((float)$msec, 3));
+		$prefix = '['.date('YmdHis', $ts).'.'.(1000 * round((float)$msec, 3));
 
 		if (!empty($_SERVER['REMOTE_ADDR'])) { 
-			$log .= '@'.$_SERVER['REMOTE_ADDR'];
+			$prefix .= '@'.$_SERVER['REMOTE_ADDR'];
 		}
 
-		if (isset($_SERVER['SCRIPT_FILENAME']) && isset($_SERVER['QUERY_STRING'])) {
-		  $log .= '] '.$_SERVER['SCRIPT_FILENAME'].'?'.$_SERVER['QUERY_STRING']."\n$msg";
-		}
-		else {
-			$log .= "] $msg";
-		}
-	}
-	else {
-		$log = $msg;
+		$prefix .= (isset($_SERVER['SCRIPT_FILENAME']) && isset($_SERVER['QUERY_STRING'])) ?
+			'] '.$_SERVER['SCRIPT_FILENAME'].'?'.$_SERVER['QUERY_STRING'].' - ' : '] ';
 	}
 
-	if (!empty($GLOBALS['OVERWRITE_SETTINGS']['LOG_DEBUG'])) {
-		error_log($log."\n", 3, $GLOBALS['OVERWRITE_SETTINGS']['LOG_DEBUG']);
+	if (preg_match('/^([a-zA-Z0-9_\.]+\:[0-9]+>) /', $msg, $match)) {
+		$prefix .= $match[1]."\n";
+		$msg = str_replace($match[1].' ', '',  $msg);
 	}
-	else if (mb_strlen(SETTINGS_LOG_DEBUG) > 1) {
-		error_log($log."\n", 3, SETTINGS_LOG_DEBUG);
+
+	if ($last_prefix == $prefix) {
+		$prefix = '';
 	}
 	else {
-		error_log($log);
+		$last_prefix = $prefix;
+		$prefix = '··· '.$prefix;
+	}
+
+	if ($log_to == 'print') {
+		print $prefix.$msg."\n";
+	}
+	else if (mb_strlen($log_to) > 1) {
+		error_log($prefix.$msg."\n", 3, $log_to);
+	}
+	else {
+		error_log($prefix.$msg."\n");
 	}
 }
+
 
