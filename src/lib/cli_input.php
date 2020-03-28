6c643de04624c6836dd7003ee1364cc7cc4c9a6c
Roland Kujundzic <roland@kujundzic.de>
2020-03-28 11:12:58 +0100

    cli_http_input added

--- src/lib/cli_input.php
+++ src/lib/cli_input.php
@@ -2,66 +2,143 @@
 
 namespace rkphplib\lib;
 
+require_once dirname(__DIR__).'/Exception.class.php';
+
+use rkphplib\Exception;
 
 /**
- * Execute only once (define SETTINGS_CLI_INPUT) in cli mode.
- * Export $_SERVER[argv] input into $_REQUEST and $_SERVER. 
- * Export --req:name=value or --req:list[]=value1 as $_REQUEST[name]=value (do not overwrite already set keys).
- * Export --http:name as $_SERVER[HTTP_NAME] (e.g. --http:host = $_SERVER[HTTP_HOST]).
- * Export --server:name as $_SERVER[SERVER_NAME] (e.g. --server:addr = $_SERVER[SERVER_ADDR]).
- * Export --srv:name= value as $_SERVER[NAME]=value (e.g. --srv:request_method = $_SERVER[REQUEST_METHOD]).
+ * Parse _$SERVER[argv]. Detect --name[=value, | name], -n[ value] and name=value.
+ *
+ * @example run.php --name value --k2 v2
+ * {"":["run.php","value","v2"],"name":"@1","k2":"@2"}
+ * @example run.php -k -n abc
+ * {"":["run.php","abc"],"k":"","n":"@1"}
+ * @example run.php -uvw xyz
+ * {"":["run.php","xyz"],"u":"","v":"","w":""}
+ * @example run.php --key v1 --key v2 -a -b x -b y
+ * {"":["run.php","v1","v2","x","y"],"key":["@1","@2"],"a":"","b":["@3","@4"]}
+ * @example run.php key=value -f -g arg
+ * {"":["run.php","arg"],"key":"value","f":"","g":"@1"}
  */
-function cli_input() : void {
+function cli_input() : ?array {
+	if (php_sapi_name() !== 'cli' || defined('SETTINGS_CLI_INPUT')) {
+		return false;
+	}
+
+	define('SETTINGS_CLI_INPUT', 1);
+	$last_key = '';
+	$res = [];
+
+	for ($i = 0; $i < count($_SERVER['argv']); $i++) {
+		$key = '';
+		$value = $_SERVER['argv'][$i];
+
+		if ($value[0] == '-' && $value[1] == '-') {
+			if (($pos = mb_strpos($value, '=')) !== false) {
+				$key = mb_substr($value, 2, $pos - 2);
+				$value = mb_substr($value, $pos + 1);
+			}
+			else {
+				$key = mb_substr($value, 2);
+				$value = (isset($res['']) && is_array($res[''])) ? '@'.count($res['']) : '@1'; 
+			}
+		}
+		else if ($value[0] == '-') {
+			$plen = strlen($value);
+
+			if ($plen == 2) {
+				$key = $value[1];
+				$value = (isset($res['']) && is_array($res[''])) ? '@'.count($res['']) : '@1';
+			}
+			else {
+				for ($k = 1; $k < $plen; $k++) {
+					$key = $value[$k];
+					if (ord($key) < 97 || ord($key) > 122) {
+						throw new Exception('invalid flag '.$key, $value);
+					}
+
+					$res[$key] = '';
+				}
+
+				$last_key = $key;
+				continue;
+			}
+		}
+		else if (preg_match('/^([a-zA-Z0-9_\.\:]+)=(.+)$/', $value, $match)) {
+			$key = $match[1];
+			$value = $match[2];
+		}
+
+		if ($value != $_SERVER['argv'][$i] && !empty($last_key) && isset($res[$last_key]) && $res[$last_key][0] == '@') {
+			$res[$last_key] = '';
+		}
+
+		$last_key = $key;
+ 
+		if (isset($res[$key])) {
+			if (!is_array($res[$key])) {
+				$res[$key] = [ $res[$key] ];
+			}
+
+			array_push($res[$key], $value);
+		}
+		else {
+			$res[$key] = $value;
+		}
+	}
+ 
+	return $res;
+}
+
+
+/**
+ * Run cli_input and export output into $_REQUESTE (prefix req:) and $_SERVER (prefix srv|server|http:).
+ *
+ * @example run2.php --req:name value --req:list=v1 req:list=v2
+ * REQUEST={"name":"value","list":["v1","v2"]}
+ * SERVER={}
+ *
+ * @example run2.php req:a=x --req:b http:host=domain.tld server:addr=1.2.3.4 --srv:request_method post
+ * REQUEST={"a":"x","b":1}
+ * SERVER={"HTTP_HOST":"domain.tld","SERVER_ADDR":"1.2.3.4","REQUEST_METHOD":"post"}
+ */
+function cli_http_input() : void {
 	if (php_sapi_name() !== 'cli' || defined('SETTINGS_CLI_INPUT')) {
 		return;
 	}
 
-	define('SETTINGS_CLI_INPUT', 1);
+	$data = cli_input();
 
-	$is_req = [];
-
-	foreach ($_SERVER['argv'] as $parameter) {
-		if (mb_substr($parameter, 0, 6) == '--req:' && ($pos = mb_strpos($parameter, '=')) > 0) {
-			$name = mb_substr($parameter, 6, $pos - 6);
-			$value = mb_substr($parameter, $pos + 1);
-			$is_array = false;
-
-			if (mb_substr($name, -2) == '[]') {
-				$name = mb_substr($name, 0, -2);
-				$is_array = true;
-			}
-
-			if (!isset($_REQUEST[$name]) || isset($is_req[$name])) {
-				if ($is_array) {
-					if (!isset($_REQUEST[$name])) {
-						$_REQUEST[$name] = [];
-					}
-
-					array_push($_REQUEST[$name], $value);
-				}
-				else {
-					$_REQUEST[$name] = $value;
-				}
-
-				$is_req[$name] = 1;
-			}
+	foreach ($data as $key => $value) {
+		if (empty($key)) {
+			continue;
 		}
-		else if (mb_substr($parameter, 0, 7) == '--http:' && ($pos = mb_strpos($parameter, '=')) > 0) {
-			$name = 'HTTP_'.strtoupper(mb_substr($parameter, 6, $pos - 6));
-			if (!isset($_SERVER[$name])) {
-				$_SERVER[$name] = mb_substr($parameter, $pos + 1);
-			}
+
+		if (empty($value)) {
+			$value = 1;
 		}
-		else if (mb_substr($parameter, 0, 9) == '--server:' && ($pos = mb_strpos($parameter, '=')) > 0) {
-			$name = 'SERVER_'.strtoupper(mb_substr($parameter, 9, $pos - 9));
-			if (!isset($_SERVER[$name])) {
-				$_SERVER[$name] = mb_substr($parameter, $pos + 1);
-			}
+		else if ($value[0] == '@') {
+			$n = intval(substr($value, 1));
+			$value = $data[''][$n];
 		}
-		else if (mb_substr($parameter, 0, 6) == '--srv:' && ($pos = mb_strpos($parameter, '=')) > 0) {
-			$name = strtoupper(mb_substr($parameter, 6, $pos - 6));
-			if (!isset($_SERVER[$name])) {
-				$_SERVER[$name] = mb_substr($parameter, $pos + 1);
+
+		if (substr($key, 0, 4) == 'req:') {
+			$key = substr($key, 4);
+			$_REQUEST[$key] = $value;
+		}
+		else {
+			if (substr($key, 0, 4) == 'srv:') {
+				$key = strtoupper(substr($key, 4));
+			}
+			else if (substr($key, 0, 5) == 'http:') {
+				$key = 'HTTP_'.strtoupper(substr($key, 5));
+			}
+			else if (substr($key, 0, 7) == 'server:') {
+				$key = 'SERVER_'.strtoupper(substr($key, 7));
+			}
+
+			if (!isset($_SERVER[$key])) {
+				$_SERVER[$key] = $value;
 			}
 		}
 	}
