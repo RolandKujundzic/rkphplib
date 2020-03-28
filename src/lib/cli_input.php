f668836d0e17a5381937d5d4fbcbb6eaac0f2674
Roland Kujundzic <roland@inkoeln.com>
2020-03-28 19:34:31 +0100

    @file and @json added

--- src/lib/cli_input.php
+++ src/lib/cli_input.php
@@ -8,6 +8,8 @@ use rkphplib\Exception;
 
 /**
  * Parse _$SERVER[argv]. Detect --name[=value, | name], -n[ value] and name=value.
+ * Use key @file=path/to/file to parse file into result (suffix .ser|.conf|.json).
+ * Use @file to json file and @json to load json string.
  *
  * @example run.php --name value --k2 v2
  * {"":["run.php","value","v2"],"name":"@1","k2":"@2"}
@@ -19,21 +21,44 @@ use rkphplib\Exception;
  * {"":["run.php","v1","v2","x","y"],"key":["@1","@2"],"a":"","b":["@3","@4"]}
  * @example run.php key=value -f -g arg
  * {"":["run.php","arg"],"key":"value","f":"","g":"@1"}
+ * @example run.php '@file:test=test.json' # test.json: {"hash":{"a":"aa","b":"bbb"},"list":["a","b"]}
+ * {"":"run.php","test":{"hash":{"a":"aa","b":"bbb"},"list":["a","b"]}}
+ * @example run.php '@json:={"k1":"v1","k2":["a","b"]}'
+ * {"":"run.php","k1":"v1","k2":["a","b"]}
  */
 function cli_input() : ?array {
-	if (php_sapi_name() !== 'cli' || defined('SETTINGS_CLI_INPUT')) {
+	if (php_sapi_name() !== 'cli') {
 		return false;
 	}
 
-	define('SETTINGS_CLI_INPUT', 1);
 	$last_key = '';
 	$res = [];
 
 	for ($i = 0; $i < count($_SERVER['argv']); $i++) {
 		$key = '';
 		$value = $_SERVER['argv'][$i];
+	
+		if ($value[0] == '@' && ($pos = mb_strpos($value, '=')) !== false) {
+			if (mb_substr($value, 0, 6) == '@file:') {
+				$key = mb_substr($value, 6, $pos - 6);
+				$value = json_decode(file_get_contents(mb_substr($value, $pos + 1)), true);
+			}
+			else if (mb_substr($value, 0, 6) == '@json:') {
+				$key = mb_substr($value, 6, $pos - 6);
+				$value = json_decode(mb_substr($value, $pos + 1), true);
+			}
 
-		if ($value[0] == '-' && $value[1] == '-') {
+			if (empty($key)) {
+				$res = array_merge($res, $value);
+			}
+			else {
+				$res[$key] = $value;
+			}
+
+			$last_key = $key;
+			continue;
+		}
+		else if ($value[0] == '-' && $value[1] == '-') {
 			if (($pos = mb_strpos($value, '=')) !== false) {
 				$key = mb_substr($value, 2, $pos - 2);
 				$value = mb_substr($value, $pos + 1);
@@ -64,7 +89,7 @@ function cli_input() : ?array {
 				continue;
 			}
 		}
-		else if (preg_match('/^([a-zA-Z0-9_\.\:]+)=(.+)$/', $value, $match)) {
+		else if (preg_match('/^([a-zA-Z0-9_\.\:\[\]]+)=(.+)$/', $value, $match)) {
 			$key = $match[1];
 			$value = $match[2];
 		}
@@ -87,6 +112,7 @@ function cli_input() : ?array {
 		}
 	}
  
+	\rkphplib\lib\log_debug("cli_input:115> _SERVER[argv]: ".print_r($_SERVER['argv'], true)."\nresult: ".print_r($res, true));
 	return $res;
 }
 
@@ -103,7 +129,7 @@ function cli_input() : ?array {
  * SERVER={"HTTP_HOST":"domain.tld","SERVER_ADDR":"1.2.3.4","REQUEST_METHOD":"post"}
  */
 function cli_http_input() : void {
-	if (php_sapi_name() !== 'cli' || defined('SETTINGS_CLI_INPUT')) {
+	if (php_sapi_name() !== 'cli') {
 		return;
 	}
 
@@ -122,8 +148,11 @@ function cli_http_input() : void {
 			$value = $data[''][$n];
 		}
 
+		$value_str = is_array($value) ? print_r($value, true) : $value;
+
 		if (substr($key, 0, 4) == 'req:') {
 			$key = substr($key, 4);
+			\rkphplib\lib\log_debug("cli_http_input:155> set _REQUEST[$key]=[$value_str]");
 			$_REQUEST[$key] = $value;
 		}
 		else {
@@ -138,6 +167,7 @@ function cli_http_input() : void {
 			}
 
 			if (!isset($_SERVER[$key])) {
+				\rkphplib\lib\log_debug("cli_http_input:170> set _SERVER[$key]=[$value_str]");
 				$_SERVER[$key] = $value;
 			}
 		}
