fda16a3cabb274af572adbb1dfe27c11c9deea9a
Roland Kujundzic <roland@inkoeln.com>
2020-05-25 15:41:06 +0200

    initial

---
+++ test/XML/tn.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../../src/XML.class.php';
+
+if (empty($_SERVER['argv'][1]) || intval($_SERVER['argv'][1]) < 1) {
+	print "\nSYNTAX: php {$_SERVER['argv'][0]} N\n\nN=1 execute in/t1.php\n\n";
+	exit;
+}
+
+ob_start();
+include "in/t{$_SERVER['argv'][1]}.php";
+$out = ob_get_contents();
+ob_end_clean();
+
+print $out;
