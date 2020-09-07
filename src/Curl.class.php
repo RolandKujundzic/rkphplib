48947ea38b3105287c2a46da6227054ff69a95a9
Roland Kujundzic <roland@inkoeln.com>
2020-09-07 14:54:42 +0200

    fix typo

--- src/Curl.class.php
+++ src/Curl.class.php
@@ -2,7 +2,7 @@
 
 namespace rkphplib;
 
-require_once __DIR__.'/traits/Options.class.php';
+require_once __DIR__.'/traits/Options.php';
 require_once __DIR__.'/JSON.class.php';
 require_once __DIR__.'/File.class.php';
 require_once __DIR__.'/Dir.class.php';
