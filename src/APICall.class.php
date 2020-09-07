29d0cf02d6066c984eba724f44004be22de5026e
Roland Kujundzic <roland@inkoeln.com>
2020-09-07 14:50:31 +0200

    refactoring

--- src/APICall.class.php
+++ src/APICall.class.php
@@ -7,7 +7,7 @@ require_once __DIR__.'/JSON.class.php';
 require_once __DIR__.'/XML.class.php';
 require_once __DIR__.'/File.class.php';
 require_once __DIR__.'/Dir.class.php';
-require_once __DIR__.'/traits/Map.trait.php';
+require_once __DIR__.'/traits/Map.php';
 
 
 /**
