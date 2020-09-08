f41a2d780ede9e7a0c5c2e5e14753a12cdca1cc9
Roland Kujundzic <roland@inkoeln.com>
2020-09-08 12:15:05 +0200

    log_debug

--- src/tok/TConf.class.php
+++ src/tok/TConf.class.php
@@ -5,7 +5,7 @@ namespace rkphplib\tok;
 $parent_dir = dirname(__DIR__);
 require_once __DIR__.'/TokPlugin.iface.php';
 require_once $parent_dir.'/Database.class.php';
-require_once $parent_dir.'/traits/Map.trait.php';
+require_once $parent_dir.'/traits/Map.php';
 require_once $parent_dir.'/lib/conf2kv.php';
 require_once $parent_dir.'/lib/kv2conf.php';
 
