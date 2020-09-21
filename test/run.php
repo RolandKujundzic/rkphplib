5b65a6ebf2d248376a4bce1ff04df1a223ff5c6c
Roland Kujundzic <roland@inkoeln.com>
2020-09-21 22:14:50 +0200

    refactoring

--- test/run.php
+++ test/run.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once dirname(__DIR__).'/src/TestHelper.class.php';
-
-$th = new rkphplib\TestHelper();
+require_once __DIR__.'/settings.php';
 
 // ob_* is necessary because of Session/run.php
 ob_start();
