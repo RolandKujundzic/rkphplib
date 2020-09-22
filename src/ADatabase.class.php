4f55caf4eba530825795ee510341023bbeabb9be
Roland Kujundzic <roland@inkoeln.com>
2020-09-22 12:09:35 +0200

    log_debug

--- src/ADatabase.class.php
+++ src/ADatabase.class.php
@@ -496,7 +496,7 @@ public function hasQuery(string $qkey, string $query = '') : bool {
  * all queries must be same.
  */
 public function hasQueries(array $query_map) : bool {
-	// \rkphplib\lib\log_debug("ADatabase.hasQueries:489> query_map: ".print_r($query_map, true));
+	// \rkphplib\lib\log_debug("ADatabase.hasQueries:499> query_map: ".print_r($query_map, true));
 	if (!is_array($query_map)) {
 		return false;
 	}
@@ -1484,7 +1484,7 @@ public function buildQuery(string $table, string $type, array $kv = []) : string
 
 	$add_default = empty($kv['@add_default']) ? false : true;
 
-	// \rkphplib\lib\log_debug("ADatabase.buildQuery:1477> table=$table, type=$type, kv: ".print_r($kv, true)."p: ".join('|', array_keys($p)));
+	// \rkphplib\lib\log_debug("ADatabase.buildQuery:1487> table=$table, type=$type, kv: ".print_r($kv, true)."p: ".join('|', array_keys($p)));
 
 	foreach ($p as $col => $cinfo) {
 		$val = false;
@@ -1509,17 +1509,17 @@ public function buildQuery(string $table, string $type, array $kv = []) : string
 			}
 		}
 
-		// \rkphplib\lib\log_debug("ADatabase.buildQuery:1502> col=$col, val=$val");
+		// \rkphplib\lib\log_debug("ADatabase.buildQuery:1512> col=$col, val=$val");
 
 		if ($val !== false) {
 			array_push($key_list, self::escape_name($col));
 			array_push($val_list, $val);
-			// \rkphplib\lib\log_debug("ADatabase.buildQuery:1507> table=$table, type=$type, col=$col, val=$val");
+			// \rkphplib\lib\log_debug("ADatabase.buildQuery:1517> table=$table, type=$type, col=$col, val=$val");
 		}
 	}
 
 	if (count($key_list) == 0) {
-		// \rkphplib\lib\log_debug("ADatabase.buildQuery:1512> empty key_list - return");
+		// \rkphplib\lib\log_debug("ADatabase.buildQuery:1522> empty key_list - return");
 		return '';
 	}
 
@@ -1545,7 +1545,7 @@ public function buildQuery(string $table, string $type, array $kv = []) : string
 		throw new Exception('invalid query type - use insert|update', "table=$table type=$type"); 
 	}
 
-	// \rkphplib\lib\log_debug("ADatabase.buildQuery:1538> table=$table, type=$type, res=$res");
+	// \rkphplib\lib\log_debug("ADatabase.buildQuery:1548> table=$table, type=$type, res=$res");
 	return $res;
 }
 
--- src/tok/TBase.class.php
+++ src/tok/TBase.class.php
@@ -435,7 +435,7 @@ public function tok_tpl(array $p, ?string $arg) : string {
 		$tpl = $this->_tok->replaceTags($tpl, conf2kv($arg));
 	}
 
-	// \rkphplib\lib\log_debug("TBase.tok_tpl:439> return $tpl"); 
+	// \rkphplib\lib\log_debug("TBase.tok_tpl:438> return $tpl"); 
 	return $tpl;
 }
 
@@ -516,7 +516,7 @@ public function getVar(string $name, string $name2 = '') {
 	}
 
 	$res = $this->_tok->getVar($name);
-	// \rkphplib\lib\log_debug("TBase.getVar:520> _tok.getVar($name): ".print_r($res, true));
+	// \rkphplib\lib\log_debug("TBase.getVar:519> _tok.getVar($name): ".print_r($res, true));
 	return $res;
 }
 
@@ -644,7 +644,7 @@ private function tableRow(array $cols, array $p) : string {
 		$res .= "</table>\n";
 	}
 
-	// \rkphplib\lib\log_debug("TBase.tableRow:648> return $res");
+	// \rkphplib\lib\log_debug("TBase.tableRow:647> return $res");
 	return $res;
 }
 
@@ -836,7 +836,7 @@ public function tok_clear(string $txt) : string {
  * @tok {keep:}{find:a}{:keep} = {find:a}
  */
 public function tok_keep(string $txt) : string {
-	// \rkphplib\lib\log_debug("TBase.tok_keep:840> return $txt");
+	// \rkphplib\lib\log_debug("TBase.tok_keep:839> return $txt");
 	return $txt;
 }
 
@@ -970,7 +970,7 @@ public function tok_load(string $param, string $file) : string {
  * If SETTINGS_REQ_CRYPT is empty do not encode link.
  */
 public function tok_link(array $name_list, array $p) : string {
-	// \rkphplib\lib\log_debug("TBase.tok_link:974> name_list: ".print_r($name_list, true)."\np: ".print_r($p, true));
+	// \rkphplib\lib\log_debug("TBase.tok_link:973> name_list: ".print_r($name_list, true)."\np: ".print_r($p, true));
 	$res = 'index.php?'.SETTINGS_REQ_CRYPT.'=';
 	$keep = false;
 	$seo = '';
@@ -1069,7 +1069,7 @@ public function tok_link(array $name_list, array $p) : string {
 		$res .= self::encodeHash($p);
 	}
 
-	// \rkphplib\lib\log_debug("TBase.tok_link:1073> return $res");
+	// \rkphplib\lib\log_debug("TBase.tok_link:1072> return $res");
 	return $res;
 }
 
@@ -1159,7 +1159,7 @@ public static function decodeHash(string $data, bool $export_into_req = false) {
 		}
 	}
 
-	// \rkphplib\lib\log_debug("TBase::decodeHash:1163> return ".print_r($res, true));
+	// \rkphplib\lib\log_debug("TBase::decodeHash:1162> return ".print_r($res, true));
 	return $res;
 }
 
@@ -1453,7 +1453,7 @@ public function tok_filter(string $tag, array $filter_list) : void {
  * @see tag_filter
  */
 private function applyFilter(string $tag, string $value) : string {
-	// \rkphplib\lib\log_debug("TBase.applyFilter:1457> tag=$tag value=[$value]");
+	// \rkphplib\lib\log_debug("TBase.applyFilter:1456> tag=$tag value=[$value]");
 	if (!isset($this->_conf['filter.'.$tag])) {
 		throw new Exception('no filter', "tag=$tag value=[$value] _conf: ".print_r($this->_conf, true));
 	}
@@ -1480,7 +1480,7 @@ private function applyFilter(string $tag, string $value) : string {
 			throw new Exception('invalid filter', "tag=$tag filter=$filter value=[$value]");
 		}
 
-		// \rkphplib\lib\log_debug("TBase.applyFilter:1484> filter=$filter value=[$value]");
+		// \rkphplib\lib\log_debug("TBase.applyFilter:1483> filter=$filter value=[$value]");
 	}
 
 	return $value;
@@ -1516,7 +1516,7 @@ public function tok_esc(string $param, ?string $arg) : ?string {
 	}
 
 	if (is_null($arg) || $arg === 'null' || $arg === 'NULL') {
-		// \rkphplib\lib\log_debug("TBase.tok_esc:1520> return NULL");
+		// \rkphplib\lib\log_debug("TBase.tok_esc:1519> return NULL");
 		return 'NULL';
 	}
 
@@ -1526,7 +1526,7 @@ public function tok_esc(string $param, ?string $arg) : ?string {
 
 	$arg = $this->applyFilter('esc', $arg);
 
-	// \rkphplib\lib\log_debug("TBase.tok_esc:1530> return [$arg]");
+	// \rkphplib\lib\log_debug("TBase.tok_esc:1529> return [$arg]");
 	return $arg;
 }
 
@@ -1861,17 +1861,17 @@ public function tok_plugin(array $p) : void {
 		if (mb_strpos($plugin, ':') === false) {
 			require_once __DIR__."/$plugin.class.php";
 			$obj = '\\rkphplib\\tok\\'.$plugin;
-			// \rkphplib\lib\log_debug("TBase.tok_plugin:1865> require_once('".PATH_RKPHPLIB.'tok/'.$plugin.".class.php'); new $obj();");
+			// \rkphplib\lib\log_debug("TBase.tok_plugin:1864> require_once('".PATH_RKPHPLIB.'tok/'.$plugin.".class.php'); new $obj();");
 		}
 		else {
 			list ($path, $obj) = explode(':', $plugin);
 
 			if (basename($path) === $path && defined("PATH_$path")) {
 				$incl_path = constant("PATH_$path").'tok/'.$obj.'.class.php';
-				// \rkphplib\lib\log_debug("TBase.tok_plugin:1872> path=$path incl_path=$incl_path");
+				// \rkphplib\lib\log_debug("TBase.tok_plugin:1871> path=$path incl_path=$incl_path");
 				require_once $incl_path;
 				$obj = '\\'.strtolower($path).'\\tok\\'.$obj;
-				// \rkphplib\lib\log_debug("TBase.tok_plugin:1875> require_once('$incl_path'); new $obj();");
+				// \rkphplib\lib\log_debug("TBase.tok_plugin:1874> require_once('$incl_path'); new $obj();");
 			}
 			else {
 				throw new Exception("invalid path=[$path] or undefined PATH_$path");
@@ -2191,7 +2191,7 @@ public function tok_true(string $val, string $out) : string {
 		}
 	}
 
-	// \rkphplib\lib\log_debug('TBase.tok_true:2195> val='.print_r($val, true).' tf='.print_r($tf, true));
+	// \rkphplib\lib\log_debug('TBase.tok_true:2194> val='.print_r($val, true).' tf='.print_r($tf, true));
 	return ((is_bool($tf) && $tf) || (is_array($val) && in_array($tf, $val)) || (is_string($tf) && $tf === $val) || 
 		(is_array($tf) && !empty($val) && in_array($val, $tf))) ? $out : '';
 }
@@ -2219,7 +2219,7 @@ public function tok_false(string $out) : string {
  * Write message via log_debug.
  */
 public function tok_log(string $txt) : void {
-	\rkphplib\lib\log_debug("TBase.tok_log:2223> $txt"); // @keep
+	\rkphplib\lib\log_debug("TBase.tok_log:2222> $txt"); // @keep
 }
 
 
