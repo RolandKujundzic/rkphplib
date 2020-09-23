c0978dd681ff1ab7d756ff642b1a326b27c898b4
Roland Kujundzic <roland@inkoeln.com>
2020-09-23 23:37:58 +0200

    refactoring

--- test/lib.conf2kv/in/t1.json.php
+++ test/lib.conf2kv/in/t1.json.php
@@ -1,15 +1,7 @@
 <?php
 
-$func = 'rkphplib\lib\conf2kv';
-
-function get_example($n) {
-	$base = __DIR__.'/'.sprintf('example_%02d', $n);
-	$conf = trim(file_get_contents($base.'.conf'));
-	$ok = trim(file_get_contents($base.'.ok'));
-	return [ $conf, $ok ];
-}
-
 $test = [
+	'rkphplib\lib\conf2kv',
 	['@→oef” = pwoief=j=', '{"@→oef”":"pwoief=j="}'],
 	['first_name = Peter|#|last_name= Müller', '{"first_name":"Peter","last_name":"Müller"}'],
 	[' x y z ', '["x y z"]'],
@@ -23,11 +15,6 @@ $test = [
 	['a=A|#|b="B|#|c=C|#|d=D"|#|e=e"e', '{"a":"A","b":"\"B","c":"C","d":"D\"","e":"e\"e"}'],
 	['abc="A""BC"'."\n|#|\n\t b =  B B B", '{"abc":"A\"\"BC","b":"B B B"}'],
 	['a=1|#|b=2|#|c=3|#|', '{"a":"1","b":"2","c":"3"}'],
-	['a=@1 a, b, c|#|b=@1 a, "b, c"|#|c=@1 a\, b," c"', '{"a":["a","b","c"],"b":["a","\"b","c\""],"c":["a\\\\","b"," c"]}'],
-	get_example(1),
-	get_example(2),
-	get_example(3),
-	get_example(4),
-	get_example(5)
+	['a=@1 a, b, c|#|b=@1 a, "b, c"|#|c=@1 a\, b," c"', '{"a":["a","b","c"],"b":["a","\"b","c\""],"c":["a\\\\","b"," c"]}']
 ];
 
