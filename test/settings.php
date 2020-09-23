5439ecf3674e256c395ff0b22a5b2b24cc63de50
Roland Kujundzic <roland@inkoeln.com>
2020-09-23 23:45:25 +0200

    fix typo

--- test/settings.php
+++ test/settings.php
@@ -1,7 +1,7 @@
 <?php
 
 define('PATH_RKPHPLIB', dirname(__DIR__).'/src/');
-defined('SETTINGS_LOG_ERROR', __DIR__.'/php.fatal');
+define('SETTINGS_LOG_ERROR', __DIR__.'/php.fatal');
 
 require_once PATH_RKPHPLIB.'TestHelper.class.php';
 
