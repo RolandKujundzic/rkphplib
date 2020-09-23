dfef50e7f1187c44c984df65e899a3b7a372140f
Roland Kujundzic <roland@inkoeln.com>
2020-09-23 23:43:39 +0200

    update

--- test/settings.php
+++ test/settings.php
@@ -1,6 +1,9 @@
 <?php
 
 define('PATH_RKPHPLIB', dirname(__DIR__).'/src/');
+defined('SETTINGS_LOG_ERROR', __DIR__.'/php.fatal');
+
 require_once PATH_RKPHPLIB.'TestHelper.class.php';
+
 $th = new rkphplib\TestHelper();
 
