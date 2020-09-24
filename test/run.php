16536345b56551bf62634727acf967c93a9bcb6f
Roland Kujundzic <roland@inkoeln.com>
2020-09-24 08:30:38 +0200

    update

--- test/run.php
+++ test/run.php
@@ -3,6 +3,8 @@
 require_once __DIR__.'/settings.php';
 
 $th->test('lib.conf2kv');
+$th->test('lib.csv2kv');
+$th->test('lib.kv2conf');
 $th->test('DateCalc');
 $th->test('XMLParser');
 $th->test('tok.Tokenizer');
@@ -16,8 +18,6 @@ $res = ob_get_contents();
 ob_end_clean();
 print $res;
 
-$th->runTest('lib_csv2kv/run.php');
-$th->runTest('lib_kv2conf/run.php');
 $th->runTest('lib_split_str/run.php');
 $th->runTest('lib_array_join/run.php');
 $th->runTest('lib_csv_explode/run.php');
