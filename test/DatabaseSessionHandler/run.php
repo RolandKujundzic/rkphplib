952809ebd2f2eb0d0f06f98730faa64b917c913b
Roland Kujundzic <roland@inkoeln.com>
2020-10-11 10:51:22 +0200

    update

--- test/DatabaseSessionHandler/run.php
+++ test/DatabaseSessionHandler/run.php
@@ -2,23 +2,14 @@
 
 require_once '../settings.php';
 
-/*
- * ../../run.sh php server --port=8080
- * http://localhost:8080/run.php
- */
-
 global $th;
 
-if (!isset($th)) {
-  require_once(dirname(dirname(__DIR__)).'/src/TestHelper.class.php');
-  $th = new rkphplib\TestHelper();
-}
-
-
 $th->load('src/DatabaseSessionHandler.class.php');
-
 $sh = new \rkphplib\DatabaseSessionHandler(TEST_MYSQL);
 
-$sh->set('abc', 3);
-$th->compare('has|get()', [ $sh->has('abc'), $sh->get('abc') ], [ true, 3 ]);
+/**
+ * ToDo: fix in/t1.php cookie check (set options.http = 1 and options.ob_wrap = 0)
+ */
+
+$th->run(1, 1);
 
