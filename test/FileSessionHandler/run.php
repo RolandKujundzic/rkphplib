04ece0a7385aac56ece64ce6ef444794e307fa86
Roland Kujundzic <roland@inkoeln.com>
2020-10-11 12:01:31 +0200

    initial

---
+++ test/FileSessionHandler/run.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../settings.php';
+
+global $th;
+
+$th->load('src/FileSessionHandler.class.php');
+$sh = new \rkphplib\FileSessionHandler();
+
+/**
+ * ToDo: fix in/t1.php cookie check (set options.http = 1 and options.ob_wrap = 0)
+ */
+
+$th->run(1, 1);
+
