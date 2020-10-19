859c32ea4cbe7890f9bfdb14e9879761856dda13
Roland Kujundzic <roland@inkoeln.com>
2020-10-19 13:44:51 +0200

    initial

---
+++ test/rkphplib.settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Add your settings and copy to ~/.config/rkphplib.settings.php
+ */
+
+define('SETTINGS_MAIL_FROM', '');
+define('SETTINGS_MAIL_REPLY_TO', '');
+define('SETTINGS_SMTP_HOST', '');
+define('SETTINGS_SMTP_PORT', '587');
+define('SETTINGS_SMTP_USER', '');
+define('SETTINGS_SMTP_PASS', '');
+
