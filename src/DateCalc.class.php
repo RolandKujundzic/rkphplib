153aa347d40f099e45b36a6ae3d555f435b0fc3c
Roland Kujundzic <roland@inkoeln.com>
2020-10-19 15:08:27 +0200

    log_debug

--- src/DateCalc.class.php
+++ src/DateCalc.class.php
@@ -697,7 +697,7 @@ public static function formatDateStr(string $format_out, string $date_str, strin
  * strftime = use strftime($format_out, time)
  */
 public static function formatDateTimeStr(string $format_out, string $date_str, string $format_in = '') : string {
-	// \rkphplib\lib\log_debug("DateCalc::formatDateTimeStr:701> ('$format_out', '$date_str', '$format_in')"); 
+	// \rkphplib\lib\log_debug("DateCalc::formatDateTimeStr:700> ('$format_out', '$date_str', '$format_in')"); 
 	if ($format_in == 'unix' && mb_substr($date_str, 0, 4) == 'now(') {
 		$format_in = 'now';
 	}
