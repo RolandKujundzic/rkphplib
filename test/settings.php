febdde1e9658cf5463d0fc06a8f4c9cccfb8d05d
Roland Kujundzic <roland@inkoeln.com>
2020-10-19 13:43:23 +0200

    update

--- test/settings.php
+++ test/settings.php
@@ -6,6 +6,10 @@ define('SETTINGS_LOG_ERROR', __DIR__.'/php.fatal');
 define('SETTINGS_XCRYPT_SECRET', 'abc123'); 
 define('SETTINGS_XCRYPT_RKEY', 'xcr');
 
+if (file_exists($_SERVER['HOME'].'/.config/rkphplib.settings.php')) {
+	require_once $_SERVER['HOME'].'/.config/rkphplib.settings.php';
+}
+
 require_once PATH_RKPHPLIB.'TestHelper.class.php';
 
 $th = new rkphplib\TestHelper();
