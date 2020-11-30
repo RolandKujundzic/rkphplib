7a80fd4aa46514b6c54f184fe1267f74d1909b4f
Roland Kujundzic <roland@inkoeln.com>
2020-11-30 15:07:23 +0100

    flag 4 = detached bg job with pid check

--- src/lib/execute.php
+++ src/lib/execute.php
@@ -11,16 +11,17 @@ use rkphplib\Exception;
 /**
  * Execute shell command (e.g. rm -f {:=file}). Optional parameter $flag is int|bool|string.
  * Command parameter {:=param} are replaced with escaped (\') hash values.
- * Use {:=_param} for unescaped replace. Append [ 2>&1] to command to catch stderr. 
- * Append [> /dev/null 2> /dev/null &] for background job.
- * Append [ && echo $! > /dev/null 2>&1 & ] for background job with pid output (= return value).
+ * Use {:=_param} for unescaped replace. 
+ * Use flag=2 or append [ 2>&1] to command to catch stderr. 
+ * Use flag=4 or append [ >/dev/null 2>/dev/null &] for background job.
  * 
  * Flag is either 2^n, true|false or "test ..." string. Flag Options:
  *
  *  - true: same as 2^0
  *  - false: same as 0
  *  - 2^0: throw exception if error
- *  - 2^1: change command to "($cmd) 2>&1"
+ *  - 2^1: change command to "$cmd 2>&1"
+ *  - 2^2: detached background job "$cmd >/dev/null 2>/dev/null & echo $!"
  *  - test -f 'some/file': change command to "if test -f 'some/file'; then ($cmd) 2>&1; fi"
  *
  * Return (string|false) last line of output or true, false if not [flag & 2^0] and error.
@@ -42,6 +43,8 @@ function execute(string $cmd, ?array $parameter = null, $flag = 1) {
 		}
 	}
 
+	$check_pid = false;
+
 	if (is_bool($flag)) {
 		if ($flag) {
 			$flag = 1;
@@ -58,14 +61,18 @@ function execute(string $cmd, ?array $parameter = null, $flag = 1) {
 		$flag = 1;
 	}
   else if (is_integer($flag)) {
-		if (($flag & 2) == 2) {
-			$cmd = "($cmd) 2>&1";
+		if ($flag & 2) {
+			$cmd .= ' 2>&1';
+		}
+		else if ($flag & 4) {
+			$cmd .= ' >/dev/null 2>/dev/null & echo $!';
+			$check_pid = true;
 		}
 	}
 
 	$output_msg = '';
 
-	// \rkphplib\lib\log_debug("execute:68> cmd: $cmd");
+	// \rkphplib\lib\log_debug("execute:75> cmd: $cmd");
 	if ($cmd) {
 		$output = array();
 		$retval = -1;
@@ -88,6 +95,14 @@ function execute(string $cmd, ?array $parameter = null, $flag = 1) {
 		}
 		else {
 			$res = $output_msg;
+		}
+	}
+
+	if ($check_pid) {
+		// \rkphplib\lib\log_debug("execute:102> check pid [$cmd_out]");
+		$pid = intval($cmd_out);
+		if (!$pid || !posix_getpgid($pid)) {
+			throw new Exception('invalid external execution pid', "[$pid] Command:\r\n".$cmd."\r\nOutput:\r\n".$output_msg);
 		}
 	}
 
