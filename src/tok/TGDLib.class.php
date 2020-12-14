5f49e1c75df6f8b4482579b308d1fa898635aab7
Roland Kujundzic <roland@inkoeln.com>
2020-12-14 16:04:06 +0100

    update

--- src/tok/TGDLib.class.php
+++ src/tok/TGDLib.class.php
@@ -32,8 +32,31 @@ public function getPlugins(Tokenizer $tok) : array {
   $plugin['gdlib:init'] = TokPlugin::NO_PARAM | TokPlugin::REQUIRE_BODY | TokPlugin::KV_BODY;
   $plugin['gdlib:load'] = TokPlugin::NO_PARAM | TokPlugin::REQUIRE_BODY;
   $plugin['gdlib:new'] = TokPlugin::NO_PARAM | TokPlugin::CSLIST_BODY;
+	$plubin['gdlib:create'] = TokPlugin::NO_PARAM | TokPlugin::KV_BODY;
   $plugin['gdlib'] = 0;
   return $plugin;
+}
+
+
+/**
+ * @tok {gdlib:create}mime=image/jpeg{:gdlib}
+ * @tok {gdlib:save_as}
+ */
+public function tok_gdlib_create(array $p) : void {
+	$save_as = empty($p['save_as']) ? $this->conf['save_as'] : $p['save_as'];
+
+	if (empty($save_as)) {
+		$mime = empty($p['mime']) ? $this->conf['mime'] : $p['mime'];
+		list ($type, $suffix) = explode('/', $mime);
+		header('Content-type: '.$mime);
+		$save_as = null;
+	}
+	else {
+		list ($type, $suffix) = explode('/', File::mime($save_as)); 
+	}
+
+	${'image'.$suffix}($this->im);
+	imagedestroy($this->im);
 }
 
 
@@ -41,7 +64,7 @@ public function getPlugins(Tokenizer $tok) : array {
  * Print text. If $p[3] = fontname is empty use font_default
  * @tok {gdlib:print:80,30,Arial14}Hello{:gdlib}
  */
-public function tok_print(string $p, string $txt) : void {
+public function tok_gdlib_print(string $p, string $txt) : void {
 	if ($txt == '') {
 		return;
 	}
@@ -51,7 +74,8 @@ public function tok_print(string $p, string $txt) : void {
 	$x = intval($p[0]);
 	$y = intval($p[1]);
 
-	imagettftext($jpg_image, $font['size'], 0, $x, $y, $font['color'], $font['file'], $txt);
+	\rkphplib\lib\log_debug("TGDLib.tok_gdlib_print:71> at $x|$y: '$text'");
+	imagettftext($this->im, $font['size'], 0, $x, $y, $font['color'], $font['file'], $txt);
 }
 
 
@@ -59,29 +83,58 @@ public function tok_print(string $p, string $txt) : void {
  * Return conf.font_$name with allocated font.color
  */
 private function getFont(string $name) : array {
-	$font = $this->conf['font_'.$name];
-	$font['color'] = imagecolorallocate($this->im, $font['rgb'][0], $font['rgb'][1], $font['rgb'][2]);
+	$fkey = 'font_'.$name.'.';
+	$font = [];
+
+	$font['file'] = $this->conf[$fkey.'file'];
+	$font['size'] =	$this->conf[$fkey.'size'];
+
+	list ($r, $g, $b) = $this->getColor($this->conf['font_'.$name.'.color']);
+	$font['color'] = imagecolorallocate($this->im, $r, $g, $b);
+
+	\rkphplib\lib\log_debug([ "TGDLib.getFont:71> $name = <1>", $font ]);
 	return $font;
 }
 
 
 /**
- * Set font $name
- * @tok {gdlib:font:default}Arial.ttf, 14, #ff0000{:gdlib}
+ * Return rgb color (decimal values)
  */
-public function tok_gdlib_font(string $name, array $p) : void {
-	File::exists($this->conf['font_directory'].'/'.$p['file'], true);
-
-	if (preg_match('/^#([a-z0-9]+)([a-z0-9]+)([a-z0-9]+)$/', $p['rgb'], $match)) {
-		$p['rgb'] = [ hexdec($match[1]), hexdec($match[2]), hexdec($match[3]) ];
+private function getColor(string $color) : array {
+	if (preg_match('/^([0-9]+)\.([0-9]+)\.([0-9]+)$/', $color, $match)) {
+		$rgb = [ hexdec($match[1]), hexdec($match[2]), hexdec($match[3]) ];
+	}
+	else if (preg_match('/^#([a-z0-9]+)([a-z0-9]+)([a-z0-9]+)$/i', $color, $match)) {
+		$rgb = [ hexdec($match[1]), hexdec($match[2]), hexdec($match[3]) ];
 	}
 	else {
-		throw new Exception('invalid rgb color in font '.$name, print_r($p, true));
+		throw new Exception("invalid rgb color [$color]");
 	}
 
-	$fname = 'font_'.$name.'.';
-	foreach ($p as $key => $value) {
-		$this->conf[$fname.$key] = $value;
+	\rkphplib\lib\log_debug([ "TGDLib.getColor:90> <1>", $rgb ]);
+	return $rgb;
+}
+
+
+/**
+ * Set font $name
+ * @tok {gdlib:font:default}Arial.ttf, 14, #000000{:gdlib}
+ */
+public function tok_gdlib_font(string $name, array $p) : void {
+	$fkey = empty($name) ? 'font_default_' : 'font_'.$name.'_';
+	$default = [ '', $this->conf[$fkey.'size'], $this->conf[$fkey.'color'] ];
+	$p = array_merge($default, $p);
+
+	File::exists($this->conf['font_directory'].'/'.$p[0], true);
+
+	$fsize = intval($p[1]);
+	if ($fsize < 4 || $fsize > 100) {
+		throw new Exception('invalid font size '.$fsize);
+	}
+
+	\rkphplib\lib\log_debug([ "TGDLib.tok_gdlib_font:91> $name = <2>", $p ]);
+	foreach ($p as $name => $value) {
+		$this->conf[$fkey.$name] = $value;
 	}
 }
 
@@ -93,19 +146,24 @@ public function tok_gdlib_font(string $name, array $p) : void {
  * @tok …
  * {gdlib:init}
  * font_directory= fonts|#|
+ * font_default.size= 14|#|
+ * font_default.color= #000000|#|
  * save_as= |#|
  * load= background.jpg|#|
  * {:gdlib}
  * @eol
  */
 public function tok_gdlib_init(array $p) : void {
-	$default = [ 'font_directory' => 'fonts', 'save_as' => '' ];
+	$default = [ 'font_directory' => 'fonts', 'save_as' => '',
+		'font_default.size' => 14, 'font_default.color' => '#000000' ];
 
 	$this->conf = array_merge($default, $p);
-	$this->setFont($this->conf);
 
-	putenv('GDFONTPATH=' . realpath($this->conf['font_directory']));
+	$font_dir = realpath($this->conf['font_directory']);
+	Dir::exists($font_dir, true);
+	putenv('GDFONTPATH='.$font_dir);
 
+	\rkphplib\lib\log_debug([ "TGDLib.tok_gdlib_init:122> <1>", $this->conf ]);	
 	if (!empty($p['load'])) {
 		$this->tok_gdlib_load($p['load']);
 	}
@@ -123,10 +181,11 @@ public function tok_gdlib_init(array $p) : void {
  * @tok {gdlib:new}200,50,255.255.255{:gdlib}
  */
 private function tok_gdlib_new(array $p = []) : void {
+	\rkphplib\lib\log_debug([ "TGDLib.tok_gdlib_new:140> <1>", $p ]);	
 	$p = array_merge([ '300', '150', '#ffffff' ], $p); 
 	$this->im = imagecreatetruecolor($p[0], $p[1]);
-	$c = $this->getColor($p[2]);
-	$bgc = imagecolorallocate($this->im, $c[0], $c[1], $c[2]);
+	list ($r, $g, $b) = $this->getColor($p[2]);
+	$bgc = imagecolorallocate($this->im, $r, $g, $b);
 	imagefilledrectangle($im, 0, 0, $p[0], $p[1], $bgc);
 }
 
@@ -143,6 +202,7 @@ private function tok_gdlib_load(string $path) : void {
 		throw new Exception('invalid suffix '.$suffix, $path);
 	}
 
+	\rkphplib\lib\log_debug("TGDLib.tok_gdlib_load:161> {$call[$suffix]}($path)");	
 	$this->im = @$call[$suffix]($path);
 }
 
