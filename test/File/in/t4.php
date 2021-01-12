3325ea0efb8b96f44812996e27efe3182f441ec2
Roland Kujundzic <roland@inkoeln.com>
2021-01-12 19:40:12 +0100

    initial

---
+++ test/File/in/t4.php
@@ -0,0 +1,22 @@
+<?php
+
+class Parser {
+
+public function row(array $row) : void {
+	print 'Parser.row: ['.join('|', $row)."]\n";
+}
+
+}
+
+function parseRow(array $row) : void {
+	print 'parseRow: ['.join('|', $row)."]\n";
+}
+
+$csv = \rkphplib\File::loadCSV('in/t4.csv', '\\t');
+print_r($csv);
+
+\rkphplib\File::loadCSV('in/t4.csv', "\t", [ 'callback' => 'parseRow' ]);
+
+$parser = new Parser();
+// \rkphplib\File::loadCSV('in/t4.csv', '\\t', '"', array($parser, 'row'));
+
