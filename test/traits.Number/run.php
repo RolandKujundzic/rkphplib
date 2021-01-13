d4f930fcfc4ccf2b35e308444af3e2714165afce
Roland Kujundzic <roland@inkoeln.com>
2021-01-13 06:42:37 +0100

    initial

---
+++ test/traits.Number/run.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once '../settings.php';
+require_once PATH_RKPHPLIB.'traits/Number.php';
+
+class TraitTest {
+use \rkphplib\traits\Number;
+
+public function r(string $number, int $dp = 0) : void {
+	print $this->round($number, $dp)."\n";
+}
+
+public function pf(string $number, int $dp = 0) : void {
+	print $this->parseFloat($number, $dp)."\n";
+}
+
+public function fn(string $number) : void {
+	print $this->fixNumber($number)."\n";
+}
+
+}
+
+
+global $th;
+
+$th->run(1, 1);
+
