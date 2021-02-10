3b8d1ad09dc580239f0021cd8c7b1e5e757f86a5
Roland Kujundzic <roland@inkoeln.com>
2021-02-10 12:14:28 +0100

    log_debug

--- src/ADatabase.class.php
+++ src/ADatabase.class.php
@@ -594,7 +594,7 @@ public function hasQuery(string $qkey, string $query = '') : bool {
  * all queries must be same.
  */
 public function hasQueries(array $query_map) : bool {
-	// \rkphplib\lib\log_debug("ADatabase.hasQueries:594> query_map: ".print_r($query_map, true));
+	// \rkphplib\lib\log_debug("ADatabase.hasQueries:597> query_map: ".print_r($query_map, true));
 	if (!is_array($query_map)) {
 		return false;
 	}
@@ -1742,7 +1742,7 @@ public function buildQuery(string $table, string $type, array $kv = []) : string
 	$allow_all = empty($kv['@allow']) ? true : false;
 	$add_default = empty($kv['@add_default']) ? false : true;
 
-	// \rkphplib\lib\log_debug([ "ADatabase.buildQuery:1742> ($table, $type, …) kv: <1>\n<2>", $kv, array_keys($p) ]);
+	// \rkphplib\lib\log_debug([ "ADatabase.buildQuery:1745> ($table, $type, …) kv: <1>\n<2>", $kv, array_keys($p) ]);
 	foreach ($p as $col => $cinfo) {
 		$val = false;
 
@@ -1771,14 +1771,14 @@ public function buildQuery(string $table, string $type, array $kv = []) : string
 		}
 
 		if ($val !== false) {
-			// \rkphplib\lib\log_debug("ADatabase.buildQuery:1771> $col=[$val]");
+			// \rkphplib\lib\log_debug("ADatabase.buildQuery:1774> $col=[$val]");
 			array_push($key_list, self::escape_name($col));
 			array_push($val_list, $val);
 		}
 	}
 
 	if (count($key_list) == 0) {
-		// \rkphplib\lib\log_debug("ADatabase.buildQuery:1778> empty key_list - return");
+		// \rkphplib\lib\log_debug("ADatabase.buildQuery:1781> empty key_list - return");
 		return '';
 	}
 
@@ -1808,7 +1808,7 @@ public function buildQuery(string $table, string $type, array $kv = []) : string
 		throw new Exception('invalid query type - use insert|update', "table=$table type=$type"); 
 	}
 
-	// \rkphplib\lib\log_debug("ADatabase.buildQuery:1808> $res");
+	// \rkphplib\lib\log_debug("ADatabase.buildQuery:1811> $res");
 	return $res;
 }
 
