5c397e43fe1f2958e0e51cbdd844ea93e2efe840
Roland Kujundzic <roland@inkoeln.com>
2021-02-18 20:45:10 +0100

    initial

---
+++ test/CLI/in/t3.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once PATH_SRC.'CLI.php';
+
+use rkphplib\CLI;
+
+CLI::parse('run.php test --name=abc -uvw @req:name=value @req:list=v1 @req:list=v2');
+
+print 'CLI::argv='.print_r(CLI::$argv, true);
+print 'CLI::arg='.print_r(CLI::$arg, true);
+print '_REQUEST='.print_r($_REQUEST, true);
+
+CLI::parse('run.php @http:host=domain.tld @server:addr=1.2.3.4 @srv:request_method=post');
+print '_SERVER[HTTP_HOST]='.$_SERVER['HTTP_HOST']."\n";
+print '_SERVER[SERVER_ADDR]='.$_SERVER['SERVER_ADDR']."\n";
+print '_SERVER[REQUEST_METHOD]='.$_SERVER['REQUEST_METHOD']."\n";
+
