7e09a12a4c944d4bf2bad45ed552229c9cd89430
Roland Kujundzic <roland@inkoeln.com>
2021-02-19 19:25:28 +0100

    update

--- test/CLI/run.php
+++ test/CLI/run.php
@@ -1,7 +1,20 @@
 <?php
 
 require_once '../settings.php';
+require_once PATH_SRC.'CLI.php';
+
+use \rkphplib\CLI;
+
+function syntax(string $cmd, array $check, array $opt = []) : void {
+	$error = '';
+	CLI::$abort = 0;
+	CLI::$log = &$error;
+	CLI::parse($cmd);
+	CLI::syntax($check, $opt);
+	print 'cmd=['.join(' ', CLI::$argv).'] check=['.join('|', $check).
+		'] opt=['.join('|', $opt)."]\nlog=[".trim(CLI::$log)."]\n---\n";
+}
 
 global $th;
 
-$th->run(1, 3);
+$th->run(1, 4);
