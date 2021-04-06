dd118325b73186b1734aa2a721eb25b8138325ac
Roland Kujundzic <roland@inkoeln.com>
2021-04-06 09:29:40 +0200

    update

--- test/GDLib/in/t2.php
+++ test/GDLib/in/t2.php
@@ -6,11 +6,11 @@ $img->load('crosshair.png');
 $img->save('out/t2.png');
 $img->load('out/t2.png');
 
-print $img->get('file').': '.$img->get('width').'x'.$img->get('height').
+print $img->file.': '.$img->width.'x'.$img->height.
 	' has alpha='.$img->hasAlpha()."\n";
 
 $img->create(80, 30, '255,255,255,127');
 $img->save('out/t2a.png');
 
-print $img->get('file').': '.$img->get('width').'x'.$img->get('height').
+print $img->file.': '.$img->width.'x'.$img->height.
 	' has alpha='.$img->hasAlpha()."\n";
