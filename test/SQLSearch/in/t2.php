ef44686abc87bd39225b1c053b6d5a423adea041
Roland Kujundzic <roland@inkoeln.com>
2021-04-28 12:14:46 +0200

    update

--- test/SQLSearch/in/t2.php
+++ test/SQLSearch/in/t2.php
@@ -1,7 +1,8 @@
 <?php
 
-$search = new \rkphplib\SQLSearch([
-	'search' => 'id,name,descr',
-]);
+msearch('id,name,descr', 17);
 
+$_REQUEST['s_name_op'] = 'LLIKE';
+$_REQUEST['s_name2_op'] = 'RLIKE';
+msearch('id,name,name2,descr:LIKE', 'John', '_AND_SEARCH');
 
--- test/SQLSearch/run.php
+++ test/SQLSearch/run.php
@@ -4,4 +4,9 @@ require_once '../settings.php';
 
 global $th;
 
-$th->run(1, 1);
+function msearch(string $cols, string $val, string $query = '_WHERE_SEARCH') {
+	$search = new \rkphplib\SQLSearch([ 'search' => $cols, 'search.value' => $val ]);
+	print $search->query($query)."\n";
+}
+
+$th->run(1, 2);
