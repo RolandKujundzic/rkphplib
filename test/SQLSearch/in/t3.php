e07c75524ad3a2083d9b80ce3d2a30a7b7196b9d
Roland Kujundzic <roland@inkoeln.com>
2021-05-01 14:27:29 +0200

    initial

---
+++ test/SQLSearch/in/t3.php
@@ -0,0 +1,14 @@
+<?php
+
+$search = new \rkphplib\SQLSearch([
+	'search' => 'id,price:LE,name:LIKE'
+]);
+
+$_REQUEST = [ 's_id' => 'ID' ];
+print $search->query()."\n";
+
+$_REQUEST = [ 's_name' => 'NAME' ];
+print $search->query()."\n";
+
+$_REQUEST = [ 's_price' => '100' ];
+print $search->query()."\n";
