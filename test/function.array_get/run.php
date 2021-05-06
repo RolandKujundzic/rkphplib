d09a729381dcc285c9797c7bc7bbcbfe48ceada4
Roland Kujundzic <roland@inkoeln.com>
2021-05-06 18:44:55 +0200

    update

--- test/function.array_get/run.php
+++ test/function.array_get/run.php
@@ -2,7 +2,7 @@
 
 require_once '../settings.php';
 
-require_once __DIR__.'/../../src/function/array_get.php';
+require_once __DIR__.'/../../function/array_get.php';
 
 function call2($name, $p1, $p2) {
 	print "$name(".json_encode($p1).', '.json_encode($p2).") == ".json_encode($name($p1, $p2)).";\n";
