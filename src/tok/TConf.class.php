c730dc54aa50278e4b1ba3a56dff1b8d27eb70d8
Roland Kujundzic <roland@inkoeln.com>
2021-05-06 18:49:14 +0200

    update

--- src/tok/TConf.class.php
+++ src/tok/TConf.class.php
@@ -126,7 +126,7 @@ public function tok_conf_load(string $file) : void {
  */
 public function tok_conf(string $key, ?string $value) : string {
 	if (!is_null($this->conf)) {
-		$val = isset($this->conf[$key]) ? $this->conf[$key] : null;
+		$val = self::array_get($key, $this->conf);
 	}
 	else {
 		$qtype = ($this->lid > 0) ? 'select_user_path' : 'select_system_path';
@@ -142,6 +142,40 @@ public function tok_conf(string $key, ?string $value) : string {
 
 	// \rkphplib\lib\log_debug("TConf.tok_conf:143> [$key]=[$val]");
 	return $val;
+}
+
+
+/**
+ * @example …
+ * $x = [ 'a' => [ 'b' => 1, [ 'c' => 2 ], 'b2.c' => 3 ] ];
+ * array_get('a', $x) == [ 'b' => 1, [ 'c' => 2 ] ];
+ * array_get('a.b', $x) == 1;
+ * array_get('a.b.c', $x) == 2;
+ * array_get('a.b2.c', $x) == 3;
+ * @eol
+ * @return any
+ */
+private static function array_get(string $key, array $p) {
+  $path = explode('.', $key);
+	$res = null;
+
+  while (!is_null($p) && count($path) > 0) {
+		$pkey = join('.', $path);
+    $key = array_shift($path);
+		if (isset($p[$pkey])) {
+			$res = $p[$pkey];
+			$p = null;
+		}
+    if (isset($p[$key])) {
+			$p = $p[$key];
+			$res = $p;
+    }
+    else {
+     	$p = null;
+    }
+  }
+
+  return $res;
 }
 
 
