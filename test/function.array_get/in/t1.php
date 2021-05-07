66d18955485814284b229f2f8ee5b767ff55c566
Roland Kujundzic <roland@inkoeln.com>
2021-05-07 14:49:08 +0200

    update

--- test/function.array_get/in/t1.php
+++ test/function.array_get/in/t1.php
@@ -2,9 +2,10 @@
 
 $x = [ 'a' => [ 'b' => [ 'c' => 2, 'c.d' => 3 ], 'b2.c' => 4 ] ];
 
-call2('array_get', 'a', $x);
-call2('array_get', 'a.b', $x);
-call2('array_get', 'a.b.c', $x);
-call2('array_get', 'a.b.c.d', $x);
-call2('array_get', 'a.b2.c', $x);
+global $th;
+$th->call2('array_get', 'a', $x);
+$th->call2('array_get', 'a.b', $x);
+$th->call2('array_get', 'a.b.c', $x);
+$th->call2('array_get', 'a.b.c.d', $x);
+$th->call2('array_get', 'a.b2.c', $x);
 
