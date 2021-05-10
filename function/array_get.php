f8ef85e52c50c513a3935fa43e2379026b762e37
Roland Kujundzic <roland@inkoeln.com>
2021-05-10 16:56:12 +0200

    fix has_key.no_such_subkey bug

--- function/array_get.php
+++ function/array_get.php
@@ -1,34 +1,39 @@
 <?php
 
 /**
+ * @function array_get
  * @example …
  * $x = [ 'a' => [ 'b' => 1, [ 'c' => 2 ], 'b2.c' => 3 ] ];
  * array_get('a', $x) == [ 'b' => 1, [ 'c' => 2 ] ];
  * array_get('a.b', $x) == 1;
  * array_get('a.b.c', $x) == 2;
  * array_get('a.b2.c', $x) == 3;
+ * array_get('a.g') == null;
  * @eol
  */
 function array_get(string $key, array $p) {
-  $path = explode('.', $key);
+	$path = explode('.', $key);
 	$res = null;
 
   while (!is_null($p) && count($path) > 0) {
 		$pkey = join('.', $path);
-    $key = array_shift($path);
+		$key = array_shift($path);
+
 		if (isset($p[$pkey])) {
 			$res = $p[$pkey];
 			$p = null;
 		}
-    if (isset($p[$key])) {
+
+		if (isset($p[$key])) {
 			$p = $p[$key];
 			$res = $p;
-    }
-    else {
-     	$p = null;
-    }
-  }
+		}
+		else if (!is_null($p)) {
+			$res = null;
+			$p = null;
+		}
+	}
 
-  return $res;
+	return $res;
 }
 
