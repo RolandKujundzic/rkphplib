3087b5b5754e2d4abec8f67ab5593d2c3370049a
Roland Kujundzic <roland@inkoeln.com>
2021-05-11 09:47:46 +0200

    update tryPluginMap()

--- src/tok/Tokenizer.class.php
+++ src/tok/Tokenizer.class.php
@@ -1239,7 +1239,7 @@ public function getTag(string $name) : string {
 private function tryPluginMap(string $name) : void {
 	static $map = [
 		'Conf' => [ 'conf', 'conf:append', 'conf:get', 'conf:get_path', 'conf:id', 'conf:load', 'conf:save', 'conf:set', 'conf:set_path', 'conf:set_default', 'conf:var' ],
-		'FileSystem' => [ 'directory', 'directory:copy', 'directory:move', 'directory:create', 'directory:exists', 'directory:entries', 'directory:is', 'file', 'file:info', 'file:size', 'file:copy', 'file:exists', 'csv_file', 'csv_file:conf', 'csv_file:append', 'csv_file:open', 'csv_file:close', 'dirname', 'basename' ],
+		'FileSystem' => [ 'directory', 'directory:copy', 'directory:move', 'directory:create', 'directory:exists', 'directory:entries', 'directory:is', 'file', 'file:info', 'file:size', 'file:copy', 'file:download', 'file:exists', 'csv_file', 'csv_file:conf', 'csv_file:append', 'csv_file:open', 'csv_file:close', 'dirname', 'basename' ],
 		'FormValidator' => [ 'fv2', 'fv2:check', 'fv2:hidden', 'fv2:in', 'fv2:init', 'fv2:preset' ],
 		'Menu' => [ 'menu', 'menu:add', 'menu:conf' ],
 		'TArray' => [ 'array', 'array:set', 'array:get', 'array:shift', 'array:unshift', 'array:pop', 'array:push', 'array:join', 'array:length', 'array:split' ],
