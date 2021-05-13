b25a245d542f3d2b99c10c6253ac423d6fa33ccd
Roland Kujundzic <roland@inkoeln.com>
2021-05-13 13:04:46 +0200

    update

--- test/Hash/in/t1.php
+++ test/Hash/in/t1.php
@@ -3,11 +3,11 @@
 $x = [ 'a' => [ 'b' => [ 'c' => 2, 'c.d' => 3 ], 'b2.c' => 4 ] ];
 
 global $th;
-$th->pcall('\rkphplib\Hash::get', [ 'a' ], $x);
-$th->pcall('\rkphplib\Hash::get', [ 'a.b' ], $x);
-$th->pcall('\rkphplib\Hash::get', [ 'a.b.c' ], $x);
-$th->pcall('\rkphplib\Hash::get', [ 'a.b.c.d' ], $x);
-$th->pcall('\rkphplib\Hash::get', [ 'a.b2.c' ], $x);
-$th->pcall('\rkphplib\Hash::get', [ 'a.g' ], $x);
-$th->pcall('\rkphplib\Hash::get', [ 'x' ], $x);
+$th->pcall('\rkphplib\Hash::get', [ 'a', $x ]);
+$th->pcall('\rkphplib\Hash::get', [ 'a.b', $x ]);
+$th->pcall('\rkphplib\Hash::get', [ 'a.b.c', $x ]);
+$th->pcall('\rkphplib\Hash::get', [ 'a.b.c.d', $x ]);
+$th->pcall('\rkphplib\Hash::get', [ 'a.b2.c', $x ]);
+$th->pcall('\rkphplib\Hash::get', [ 'a.g', $x ]);
+$th->pcall('\rkphplib\Hash::get', [ 'x', $x ]);
 
