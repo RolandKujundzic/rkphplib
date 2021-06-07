50afa10d248b2cf90204ffd4eb34ed8d65a9e6c2
Roland Kujundzic <roland@inkoeln.com>
2021-06-07 22:33:38 +0200

    log_debug

--- src/Mailer.class.php
+++ src/Mailer.class.php
@@ -632,7 +632,7 @@ public function send(array $options = []) : bool {
 	$default = [ 'send' => 1, 'save' => 0, 'save_dir' => 'data/.log/mail/$date(Ym)/$date(dH)' ];
 	$options = array_merge($default, $options);
 
-	// \rkphplib\lib\log_debug([ "Mailer.send:624> <1>", $options ]);
+	// \rkphplib\lib\log_debug([ "Mailer.send:635> <1>", $options ]);
 	if (!$this->_mailer->preSend()) {
 		throw new Exception('Mailer preSend failed');
 	}
@@ -640,13 +640,13 @@ public function send(array $options = []) : bool {
 	$this->last_msg_id = null;
 
 	if (!empty($options['send'])) {
-		// \rkphplib\lib\log_debug([ "Mailer.send:632> <1>", $this->_mailer ]);
+		// \rkphplib\lib\log_debug([ "Mailer.send:643> <1>", $this->_mailer ]);
 		if (!$this->_mailer->postSend()) {
 			throw new Exception('Mailer postSend failed');
 		}
 
 		$this->last_msg_id = $this->_mailer->getLastMessageID();
-		// \rkphplib\lib\log_debug("Mailer.send:638> ".$this->last_msg_id);
+		// \rkphplib\lib\log_debug("Mailer.send:649> ".$this->last_msg_id);
 	}
 
 	if (!empty($options['save'])) {
