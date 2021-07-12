c789f40068301f6be31712e8f52255e98ac98033
Roland Kujundzic <roland@inkoeln.com>
2021-07-12 08:42:06 +0200

    initial

---
+++ test/db.Dummy/in/t3.php
@@ -0,0 +1,19 @@
+<?php
+
+function build_query(string $type, array $kv) : void {
+	$db = new \rkphplib\db\Dummy();
+	$kv['@is_null'] = [ 'comment' ];
+
+	try {
+		print $db->buildQuery('test', $type, $kv).";\n";
+	}
+	catch (\Exception $e) {
+		print "EXCEPTION\n";
+	}
+}
+
+build_query('insert', [ 'id' => 7, 'name' => 'Joe', 'comment' => 'bla' ]);
+build_query('update', [ 'id' => 7, 'name' => 'Joe', 'comment' => '' ]);
+build_query('replace', [ 'id' => 7, 'name' => 'Joe' ]);
+build_query('insert_update', [ 'id' => 7, 'name' => 'Joe' ]);
+
